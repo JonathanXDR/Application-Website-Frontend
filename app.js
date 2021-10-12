85fef90a53a48e495cb9330337bab4aee9a60807
Jonathan Russ <jonathan.russ@bluewin.ch>
2021-10-13 01:12:12 +0200

    added favicon-theme-changer

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// Get current system/browser theme
+const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+if (darkThemeMq.matches) {
+  // Theme set to dark.
+} else {
+  // Theme set to light.
+}
+
 // function to set a given theme/color-scheme
 function setTheme(themeName) {
   localStorage.setItem("theme", themeName);
@@ -12,6 +20,18 @@ function toggleTheme() {
     setTheme("dark-theme");
   }
 }
+
+// Change favicon according to theme
+const faviconTag = document.getElementById("faviconTag");
+const isDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+const changeFavicon = () => {
+  if (isDark.matches) faviconTag.href = ".favicon-dark.png";
+  else faviconTag.href = "favicon-light.png";
+};
+
+changeFavicon();
+setInterval(changeFavicon, 1000);
 
 //Hexagon Progressbar1
 var forEach = function (array, callback, scope) {
