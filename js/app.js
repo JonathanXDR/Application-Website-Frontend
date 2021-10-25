75657101391af8f45fe0fb3622268c04f6a768a0
Jonathan <jonathan.russ@bluewin.ch>
2021-10-25 06:11:29 +0200

    deleted images

--- js/app.js
+++ js/app.js
@@ -52,6 +52,23 @@ function countToggles() {
 // changeFavicon();
 // setInterval(changeFavicon, 1000);
 
+// Word Rotation
+setInterval(function () {
+  const show = document.querySelector("span[data-show]");
+  const next =
+    show.nextElementSibling || document.querySelector("span:first-child");
+  const up = document.querySelector("span[data-up]");
+
+  if (up) {
+    up.removeAttribute("data-up");
+  }
+
+  show.removeAttribute("data-show");
+  show.setAttribute("data-up", "");
+
+  next.setAttribute("data-show", "");
+}, 2000);
+
 //Hexagon Progressbar1
 var forEach = function (array, callback, scope) {
   for (var i = 0; i < array.length; i++) {
