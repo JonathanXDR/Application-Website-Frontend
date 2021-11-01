eaa39e3f6e43ed6f21e041dfcb3cdfc52136771b
JonathanXDR <jonathan.russ@bluewin.ch>
2021-11-01 14:39:12 +0100

    added changing favicon

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Get current system/browser theme
-const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)');
 if (darkThemeMq.matches) {
   // Theme set to dark.
 } else {
@@ -8,34 +8,35 @@ if (darkThemeMq.matches) {
 
 // function to set a given theme/color-scheme
 function setTheme(themeName) {
-  localStorage.setItem("theme", themeName);
+  localStorage.setItem('theme', themeName);
   document.documentElement.className = themeName;
 }
 
 // function to toggle between light and dark theme
 function toggleTheme() {
-  if (localStorage.getItem("theme") === "dark-theme") {
-    setTheme("light-theme");
+  if (localStorage.getItem('theme') === 'dark-theme') {
+    setTheme('light-theme');
   } else {
-    setTheme("dark-theme");
+    setTheme('dark-theme');
   }
+  changeFavicon();
 }
 
 // Vintage Mode
 let count = 0;
-const text = document.querySelectorAll("h1, h2, h3, h4, h5, h6, span, button");
+const text = document.querySelectorAll('h1, h2, h3, h4, h5, h6, span, button');
 
 function countToggles() {
   count++;
 
   if (count >= 10) {
-    document.getElementById("vintageModeContainer").style.display = "block";
-    text.style.fontFamily = "VT323, monospace";
-    text.style.color = "rgb(235, 235, 235)";
-    text.style.textShadow = "var(--green-glow)";
-    text.style.lineHeight = "1";
-    text.style.transform = "translateZ(100px)";
-    text.style.backfaceVisibility = "hidden";
+    document.getElementById('vintageModeContainer').style.display = 'block';
+    text.style.fontFamily = 'VT323, monospace';
+    text.style.color = 'rgb(235, 235, 235)';
+    text.style.textShadow = 'var(--green-glow)';
+    text.style.lineHeight = '1';
+    text.style.transform = 'translateZ(100px)';
+    text.style.backfaceVisibility = 'hidden';
   }
 }
 
@@ -67,33 +68,36 @@ function countToggles() {
 // };
 
 // Change favicon according to theme
-// const faviconTag = document.getElementById("faviconTag");
 // const isDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-// const changeFavicon = () => {
-//   if (isDark.matches) faviconTag.href = "/img/favicon-dark.png";
-//   else faviconTag.href = "/img/favicon-light.png";
-// };
+const changeFavicon = () => {
+  const faviconTag = document.getElementById('faviconTag');
+  if (localStorage.getItem('theme') === 'dark-theme') {
+    faviconTag.href = '/img/favicon-dark.ico';
+  } else {
+    faviconTag.href = '/img/favicon-light.ico';
+  }
+};
 
 // changeFavicon();
 // setInterval(changeFavicon, 1000);
 
 // Word Rotation
 setInterval(function () {
-  const show = document.querySelector(".mask span[data-show]");
+  const show = document.querySelector('.mask span[data-show]');
   const next =
-    show.nextElementSibling || document.querySelector(".mask span:first-child");
+    show.nextElementSibling || document.querySelector('.mask span:first-child');
 
-  const up = document.querySelector(".mask span[data-up]");
+  const up = document.querySelector('.mask span[data-up]');
 
   if (up) {
-    up.removeAttribute("data-up");
+    up.removeAttribute('data-up');
   }
 
-  show.removeAttribute("data-show");
-  show.setAttribute("data-up", "");
+  show.removeAttribute('data-show');
+  show.setAttribute('data-up', '');
 
-  next.setAttribute("data-show", "");
+  next.setAttribute('data-show', '');
 }, 2000);
 
 //Hexagon Progressbars
@@ -104,27 +108,28 @@ var forEach = function (array, callback, scope) {
 };
 
 window.onload = function () {
-  const themeButton = document.getElementById("themeId");
-  if (localStorage.getItem("theme") === "light-theme") {
-    setTheme("light-theme");
+  changeFavicon();
+  const themeButton = document.getElementById('themeId');
+  if (localStorage.getItem('theme') === 'light-theme') {
+    setTheme('light-theme');
     themeButton.checked = false;
   } else {
-    setTheme("dark-theme");
+    setTheme('dark-theme');
     themeButton.checked = true;
   }
 
   var max = 2160;
   forEach(
-    document.querySelectorAll(".hex-progress, .hex-progress2, .hex-progress3"),
+    document.querySelectorAll('.hex-progress, .hex-progress2, .hex-progress3'),
     function (index, value) {
-      percent = value.getAttribute("data-progress");
+      percent = value.getAttribute('data-progress');
       value
-        .querySelector(".fill, .fill2, .fill3")
+        .querySelector('.fill, .fill2, .fill3')
         .setAttribute(
-          "style",
-          "stroke-dashoffset: " + ((100 - percent) / 100) * max
+          'style',
+          'stroke-dashoffset: ' + ((100 - percent) / 100) * max
         );
-      value.querySelector(".value, .value2, .value3").innerHTML = percent + "%";
+      value.querySelector('.value, .value2, .value3').innerHTML = percent + '%';
     }
   );
 };
@@ -135,82 +140,82 @@ window.onload = function () {
 // shape.setAttribute("viewBox", "-250 -250 500 750");
 
 var controller = new ScrollMagic.Controller();
-TweenMax.set("#line", { visibility: 0 });
+TweenMax.set('#line', { visibility: 0 });
 
 var tweenOne = new TweenMax.fromTo(
-  "#line",
+  '#line',
   1,
-  { drawSVG: "0%" },
-  { drawSVG: "10%" }
+  { drawSVG: '0%' },
+  { drawSVG: '10%' }
 );
 
 var scene1 = new ScrollMagic.Scene({
-  triggerElement: "#line",
+  triggerElement: '#line',
 })
   .setTween(tweenOne)
   .addTo(controller);
 
 var tweenTwo = new TweenMax.fromTo(
-  "#line",
+  '#line',
   1,
-  { drawSVG: "10%" },
-  { drawSVG: "20%" }
+  { drawSVG: '10%' },
+  { drawSVG: '20%' }
 );
 
 var scene2 = new ScrollMagic.Scene({
-  triggerElement: ".dayTwo",
+  triggerElement: '.dayTwo',
 })
   .setTween(tweenTwo)
   .addTo(controller);
 
 var tweenThree = new TweenMax.fromTo(
-  "#line",
+  '#line',
   1,
-  { drawSVG: "20%" },
-  { drawSVG: "40%" }
+  { drawSVG: '20%' },
+  { drawSVG: '40%' }
 );
 
 var scene3 = new ScrollMagic.Scene({
-  triggerElement: ".dayThree",
+  triggerElement: '.dayThree',
 })
   .setTween(tweenThree)
   .addTo(controller);
 
 var tweenFour = new TweenMax.fromTo(
-  "#line",
+  '#line',
   1,
-  { drawSVG: "40%" },
-  { drawSVG: "65%" }
+  { drawSVG: '40%' },
+  { drawSVG: '65%' }
 );
 
 var scene3 = new ScrollMagic.Scene({
-  triggerElement: ".dayFour",
+  triggerElement: '.dayFour',
 })
   .setTween(tweenFour)
   .addTo(controller);
 
 var tweenFive = new TweenMax.fromTo(
-  "#line",
+  '#line',
   1,
-  { drawSVG: "65%" },
-  { drawSVG: "85%" }
+  { drawSVG: '65%' },
+  { drawSVG: '85%' }
 );
 
 var scene3 = new ScrollMagic.Scene({
-  triggerElement: ".dayFive",
+  triggerElement: '.dayFive',
 })
   .setTween(tweenFive)
   .addTo(controller);
 
 var tweenSix = new TweenMax.fromTo(
-  "#line",
+  '#line',
   1,
-  { drawSVG: "85%" },
-  { drawSVG: "102%" }
+  { drawSVG: '85%' },
+  { drawSVG: '102%' }
 );
 
 var scene3 = new ScrollMagic.Scene({
-  triggerElement: ".daySix",
+  triggerElement: '.daySix',
 })
   .setTween(tweenSix)
   .addTo(controller);
