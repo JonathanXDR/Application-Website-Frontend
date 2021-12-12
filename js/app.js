f7781f753deaa7c6590ca67068fe395a8edcda6d
Jonathan <jonathan.russ@bluewin.ch>
2021-12-12 02:16:23 +0100

    fixed svgTimeline

--- js/app.js
+++ js/app.js
@@ -118,53 +118,6 @@ setInterval(function () {
   next.setAttribute('data-show', '');
 }, 2000);
 
-// Typing Effect
-const typedTextSpan = document.querySelector('.typed-text');
-const cursorSpan = document.querySelector('.cursor');
-
-const textArray = ['hard', 'fun', 'a journey', 'LIFE'];
-const typingDelay = 200;
-const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
-
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if (!cursorSpan.classList.contains('typing'))
-      cursorSpan.classList.add('typing');
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } else {
-    cursorSpan.classList.remove('typing');
-    setTimeout(erase, newTextDelay);
-  }
-}
-
-function erase() {
-  if (charIndex > 0) {
-    if (!cursorSpan.classList.contains('typing'))
-      cursorSpan.classList.add('typing');
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(
-      0,
-      charIndex - 1
-    );
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } else {
-    cursorSpan.classList.remove('typing');
-    textArrayIndex++;
-    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-    setTimeout(type, typingDelay + 1100);
-  }
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  // On DOM Load initiate the effect
-  if (textArray.length) setTimeout(type, newTextDelay + 250);
-});
-
 //Hexagon Progressbars
 var forEach = function (array, callback, scope) {
   for (var i = 0; i < array.length; i++) {
@@ -197,80 +150,99 @@ window.onload = function () {
       value.querySelector('.value, .value2, .value3').innerHTML = percent + '%';
     }
   );
+
+  timelineScrolling();
 };
 
 // Timeline
-let ticking = false;
-let last_known_scroll_position = 0;
-let updatePath = false;
+function timelineScrolling() {
+  let ticking = false;
+  let last_known_scroll_position = 0;
+  let updatePath = false;
 
-const element = document.getElementById('svgTimeline');
-const path = element.querySelector('path');
-let totalLength = 0;
+  const element = document.getElementById('svgTimeline');
+  const path = element.querySelector('path');
+  let totalLength = 0;
 
-initPath(path);
+  const timelineItems = document.querySelectorAll('.timeline__item');
+  let timelineItemHeights = [];
+  timelineItems.forEach((item) => {
+    timelineItemHeights.push(item.offsetHeight);
+  });
+  timelineItemHeights.forEach((height) => {
+    totalLength += height;
+  });
 
-function initPath(path) {
-  totalLength = path.getTotalLength();
-  path.style.strokeDasharray = `${totalLength}`;
-  path.style.strokeDashoffset = totalLength;
-}
+  console.log(totalLength);
+  // path.setAttribute('stroke-dasharray', totalLength);
 
-function handleEntries(entries) {
-  console.log(entries);
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      console.log(entry.target);
+  // const iSize = parseInt(this.size);
+
+  // svg.setAttribute('viewBox', '0 0 ' + iSize + ' ' + iSize);
+
+  initPath(path);
+
+  function initPath(path) {
+    totalLength = path.getTotalLength();
+    path.style.strokeDasharray = `${totalLength}`;
+    path.style.strokeDashoffset = totalLength;
+  }
+
+  function handleEntries(entries) {
+    console.log(entries);
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        console.log(entry.target);
+      }
+    });
+  }
+
+  let observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log(entry);
+          updatePath = true;
+        } else {
+          updatePath = false;
+        }
+      });
+    },
+    { rootMargin: '0px 0px 0px 0px' }
+  );
+
+  observer.observe(element);
+
+  function doSomething(scroll_pos) {
+    if (!updatePath) {
+      return;
+    }
+    window.requestAnimationFrame(() => {
+      const center = window.innerHeight / 2;
+      const boundaries = path.getBoundingClientRect();
+      const top = boundaries.top;
+      const height = boundaries.height;
+      const percentage = (center - top) / height;
+      const drawLength = percentage > 0 ? totalLength * percentage : 0;
+      path.style.strokeDashoffset =
+        drawLength < totalLength ? totalLength - drawLength : 0;
+    });
+  }
+
+  window.addEventListener('scroll', function (e) {
+    last_known_scroll_position = window.scrollY;
+
+    if (!ticking) {
+      window.requestAnimationFrame(function () {
+        doSomething(last_known_scroll_position);
+        ticking = false;
+      });
+
+      ticking = true;
     }
   });
 }
-
-let observer = new IntersectionObserver(
-  (entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        console.log(entry);
-        updatePath = true;
-      } else {
-        updatePath = false;
-      }
-    });
-  },
-  { rootMargin: '0px 0px 0px 0px' }
-);
-
-observer.observe(element);
-
-function doSomething(scroll_pos) {
-  if (!updatePath) {
-    return;
-  }
-  window.requestAnimationFrame(() => {
-    const center = window.innerHeight / 2;
-    const boundaries = path.getBoundingClientRect();
-    const top = boundaries.top;
-    const height = boundaries.height;
-    const percentage = (center - top) / height;
-    const drawLength = percentage > 0 ? totalLength * percentage : 0;
-    path.style.strokeDashoffset =
-      drawLength < totalLength ? totalLength - drawLength : 0;
-  });
-}
-
-window.addEventListener('scroll', function (e) {
-  last_known_scroll_position = window.scrollY;
-
-  if (!ticking) {
-    window.requestAnimationFrame(function () {
-      doSomething(last_known_scroll_position);
-      ticking = false;
-    });
-
-    ticking = true;
-  }
-});
-
 // Check if music cover is visible in viewport
 // function isInViewport(el) {
 //   const rect = el.getBoundingClientRect();
