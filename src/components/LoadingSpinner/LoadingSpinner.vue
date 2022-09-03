056258e16ee6d6f87adb718179584c293e484ef7
Jonathan Russ <jonathan.russ@bluewin.ch>
2022-09-03 18:41:32 +0200

    Add LoadingSpinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="_gears-loading">
+    <div class="[object Object] loading">
+      <span aria-live="assertive" class="visuallyhidden">Loading..</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./LoadingSpinner.ts"></script>
+<style src="./LoadingSpinner.css"></style>
