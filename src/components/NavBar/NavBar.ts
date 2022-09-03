c67e2d12d6e1846c3806ea1c41b7d764f1e5a2e8
Jonathan Russ <jonathan.russ@bluewin.ch>
2022-09-03 18:40:51 +0200

    Add NavBar component

---
+++ src/components/NavBar/NavBar.ts
@@ -0,0 +1,98 @@
+import LogoIcon from '@/components/Icons/LogoIcon.vue';
+
+export default {
+  name: 'NavBar',
+  components: {
+    LogoIcon,
+  },
+  data() {
+    return {
+      items: [
+        { name: 'About', route: '#about' },
+        { name: 'Sprachkenntnisse', route: '#languages' },
+        { name: 'Referenzen', route: '#references' },
+        { name: 'Anderes', route: '#other' },
+        { name: 'Technologien', route: '#technologies' },
+        { name: 'Projekte', route: '#projects' },
+      ],
+    };
+  },
+  methods: {
+    storeTheme(themeName: string): void {
+      localStorage.setItem('theme', themeName);
+      document.documentElement.className = themeName;
+    },
+
+    toggleTheme(): void {
+      if (localStorage.getItem('theme') === 'dark') {
+        this.storeTheme('light');
+      } else {
+        this.storeTheme('dark');
+      }
+    },
+
+    themeSettings(): void {
+      const themeButton = document.getElementById(
+        'theme-checkbox'
+      ) as HTMLInputElement;
+
+      if (localStorage.getItem('theme') === 'light') {
+        this.storeTheme('light');
+        themeButton.checked = false;
+      } else {
+        this.storeTheme('dark');
+        themeButton.checked = true;
+      }
+
+      const preferedTheme = window.matchMedia('(prefers-color-scheme: dark)');
+      if (preferedTheme.matches) {
+        this.storeTheme('dark');
+        themeButton.checked = true;
+      } else {
+        this.storeTheme('light');
+        themeButton.checked = false;
+      }
+
+      themeButton.addEventListener('change', function () {
+        const elementsAndClassNames = [
+          {
+            element: document.getElementById('ac-ln-background'),
+            className: 'ac-ln-background-transition',
+          },
+          {
+            element: document.getElementById('ribbon-content-wrapper'),
+            className: 'ribbon-content-wrapper-animation',
+          },
+          {
+            element: document.getElementById('ribbon-content'),
+            className: 'ribbon-content-animation',
+          },
+          {
+            element: document.getElementById('ribbon-link'),
+            className: 'ribbon-link-animation',
+          },
+        ] as Array<{ element: HTMLElement; className: string }>;
+
+        elementsAndClassNames.forEach((elementAndClassName) => {
+          elementAndClassName.element?.classList.remove(
+            elementAndClassName.className
+          );
+        });
+
+        setTimeout(() => {
+          elementsAndClassNames[0].element.classList.add(
+            elementsAndClassNames[0].className
+          );
+        }, 500);
+
+        setTimeout(() => {
+          elementsAndClassNames.forEach((elementAndClassName) => {
+            elementAndClassName.element?.classList.add(
+              elementAndClassName.className
+            );
+          });
+        }, 1);
+      });
+    },
+  },
+};
---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <input type="checkbox" id="ac-ln-menustate" />
+  <div id="ac-ln-sticky-placeholder" class="ac-ln-sticking"></div>
+  <nav
+    id="ac-localnav"
+    class="ac-localnav-dark ac-localnav-noborder ac-ln-sticking"
+  >
+    <div class="ac-ln-wrapper">
+      <div
+        id="ac-ln-background"
+        class="ac-ln-background ac-ln-background-transition"
+      ></div>
+      <div class="ac-ln-content">
+        <a href="https://jonathan-russ.com" class="ac-ln-title"
+          ><LogoIcon />
+        </a>
+        <div class="ac-ln-menu">
+          <div class="ac-ln-menu-tray">
+            <ul class="ac-ln-menu-items">
+              <li
+                v-for="(item, index) in items"
+                :key="index"
+                class="ac-ln-menu-item"
+              >
+                <router-link :to="item.route" class="ac-ln-menu-link">
+                  {{ item.name }}
+                </router-link>
+              </li>
+            </ul>
+          </div>
+          <div class="ac-ln-actions">
+            <div class="ac-ln-action ac-ln-action-menucta">
+              <label for="ac-ln-menustate" class="ac-ln-menucta">
+                <span class="ac-ln-menucta-chevron"></span>
+              </label>
+            </div>
+            <div id="theme-button">
+              <input
+                type="checkbox"
+                @click="
+                  toggleTheme();
+                  countToggles();
+                "
+                class="btn-input"
+                id="theme-checkbox"
+              />
+              <span></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <label id="ac-ln-curtain" for="ac-ln-menustate"></label>
+</template>
+
+<script lang="ts" src="./NavBar.ts"></script>
+<style src="./NavBar.css"></style>
