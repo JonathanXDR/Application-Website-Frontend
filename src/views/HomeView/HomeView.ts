ba7e9db760dfe8d77494e5270c491a6937f8b8b9
Jonathan Russ <jonathan.russ@bluewin.ch>
2022-10-03 20:03:24 +0200

    Add defineComponent

--- src/views/HomeView/HomeView.ts
+++ src/views/HomeView/HomeView.ts
@@ -1,3 +1,5 @@
+import { defineComponent } from 'vue';
+
 import AirplaneDepartureIcon from '@/components/Icons/AirplaneDepartureIcon.vue';
 import ArrowDownCircleIcon from '@/components/Icons/ArrowDownCircleIcon.vue';
 import CalendarIcon from '@/components/Icons/CalendarIcon.vue';
@@ -12,7 +14,7 @@ import ArticleItem from '@/components/ArticleItem/ArticleItem.vue';
 
 import json from '@/assets/data/data.json';
 
-export default {
+export default defineComponent({
   name: 'HomeView',
   components: {
     AirplaneDepartureIcon,
@@ -32,101 +34,85 @@ export default {
     };
   },
   created() {
-    function timelineScrolling() {
-      let ticking = false;
-      let last_known_scroll_position = 0;
-      let updatePath = false;
-
-      const element = document.getElementById('svg-timeline') as HTMLElement;
-      const path = element.querySelector('path') as SVGPathElement;
-      let totalLength = 0;
-
-      let timelineHeight = 0;
-
-      setTimeout(() => {
-        timelineHeight = document.getElementById('timeline-id')
-          .offsetHeight as number;
-        // console.log(Math.ceil(timelineHeight));
-        const size = parseInt(Math.ceil(timelineHeight));
-        // console.log(size);
-
-        element.setAttribute('viewBox', `0 0 8 ${size}`);
-        element.setAttribute('height', size);
-        element.setAttribute('xmlns', `http://www.w3.org/${size}/svg`);
-
-        path.setAttribute('d', `M 4 0 L 4 ${size}`);
-
-        path.setAttribute('stroke-dasharray', totalLength);
-
-        initPath(path);
-
-        function initPath(path: any) {
-          totalLength = path.getTotalLength();
-          path.style.strokeDasharray = `${totalLength}`;
-          path.style.strokeDashoffset = totalLength;
-        }
-
-        function handleEntries(entries: any) {
-          console.log(entries);
-          entries.forEach((entry: any) => {
-            console.log(entry);
-            if (entry.isIntersecting) {
-              console.log(entry.target);
-            }
-          });
-        }
-
-        const observer = new IntersectionObserver(
-          (entries, observer) => {
-            entries.forEach((entry) => {
-              if (entry.isIntersecting) {
-                // console.log(entry);
-                updatePath = true;
-              } else {
-                updatePath = false;
-              }
-            });
-          },
-          { rootMargin: '0px 0px 0px 0px' }
-        );
-
-        observer.observe(element);
-
-        function doSomething() {
-          if (!updatePath) {
-            return;
-          }
-          window.requestAnimationFrame(() => {
-            const center = window.innerHeight / 2;
-            const boundaries = path.getBoundingClientRect();
-            const top = boundaries.top;
-            const height = boundaries.height;
-            const percentage = (center - top) / height;
-            const drawLength = percentage > 0 ? totalLength * percentage : 0;
-            path.style.strokeDashoffset =
-              drawLength < totalLength ? totalLength - drawLength : 0;
-          });
-        }
-
-        window.addEventListener('scroll', function (e) {
-          last_known_scroll_position = window.scrollY;
-
-          if (!ticking) {
-            window.requestAnimationFrame(function () {
-              doSomething(last_known_scroll_position);
-              ticking = false;
-            });
-
-            ticking = true;
-          }
-        });
-      }, 500);
-    }
-
-    if (document.getElementById('timeline') === true) {
-      const timelineHeight =
-        document.getElementById('timeline-id').offsetHeight;
-      console.log(timelineHeight);
-    }
+    // function timelineScrolling() {
+    //   let ticking = false;
+    //   let last_known_scroll_position = 0;
+    //   let updatePath = false;
+    //   const element = document.getElementById('svg-timeline') as HTMLElement;
+    //   const path = element.querySelector('path') as SVGPathElement;
+    //   let totalLength = 0;
+    //   let timelineHeight = 0;
+    //   setTimeout(() => {
+    //     timelineHeight = document.getElementById('timeline-id')
+    //       .offsetHeight as number;
+    //     // console.log(Math.ceil(timelineHeight));
+    //     const size = parseInt(Math.ceil(timelineHeight));
+    //     // console.log(size);
+    //     element.setAttribute('viewBox', `0 0 8 ${size}`);
+    //     element.setAttribute('height', size);
+    //     element.setAttribute('xmlns', `http://www.w3.org/${size}/svg`);
+    //     path.setAttribute('d', `M 4 0 L 4 ${size}`);
+    //     path.setAttribute('stroke-dasharray', totalLength);
+    //     initPath(path);
+    //     function initPath(path: any) {
+    //       totalLength = path.getTotalLength();
+    //       path.style.strokeDasharray = `${totalLength}`;
+    //       path.style.strokeDashoffset = totalLength;
+    //     }
+    //     function handleEntries(entries: any) {
+    //       console.log(entries);
+    //       entries.forEach((entry: any) => {
+    //         console.log(entry);
+    //         if (entry.isIntersecting) {
+    //           console.log(entry.target);
+    //         }
+    //       });
+    //     }
+    //     const observer = new IntersectionObserver(
+    //       (entries, observer) => {
+    //         entries.forEach((entry) => {
+    //           if (entry.isIntersecting) {
+    //             // console.log(entry);
+    //             updatePath = true;
+    //           } else {
+    //             updatePath = false;
+    //           }
+    //         });
+    //       },
+    //       { rootMargin: '0px 0px 0px 0px' }
+    //     );
+    //     observer.observe(element);
+    //     function doSomething() {
+    //       if (!updatePath) {
+    //         return;
+    //       }
+    //       window.requestAnimationFrame(() => {
+    //         const center = window.innerHeight / 2;
+    //         const boundaries = path.getBoundingClientRect();
+    //         const top = boundaries.top;
+    //         const height = boundaries.height;
+    //         const percentage = (center - top) / height;
+    //         const drawLength = percentage > 0 ? totalLength * percentage : 0;
+    //         path.style.strokeDashoffset =
+    //           drawLength < totalLength ? totalLength - drawLength : 0;
+    //       });
+    //     }
+    //     window.addEventListener('scroll', function (e) {
+    //       last_known_scroll_position = window.scrollY;
+    //       if (!ticking) {
+    //         window.requestAnimationFrame(function () {
+    //           doSomething(last_known_scroll_position);
+    //           ticking = false;
+    //         });
+    //         ticking = true;
+    //       }
+    //     });
+    //   }, 500);
+    // }
+    // if (document.getElementById('timeline') === true) {
+    //   const timelineHeight =
+    //     document.getElementById('timeline-id').offsetHeight;
+    //   console.log(timelineHeight);
+    // }
   },
-};
+});
