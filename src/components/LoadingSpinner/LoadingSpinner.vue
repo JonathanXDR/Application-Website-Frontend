76ff40ffc7f66c41ae97ce8d650c8c3cb7078e29
Jonathan Russ <jonathan.russ@bluewin.ch>
2022-10-14 17:31:43 +0200

    Update LoadingSpinner component

--- src/components/LoadingSpinner/LoadingSpinner.vue
+++ src/components/LoadingSpinner/LoadingSpinner.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="_gears-loading">
-    <div class="[object Object] loading">
+  <div class="gears-loading">
+    <div class="loading">
       <span aria-live="assertive" class="visuallyhidden">Loading..</span>
     </div>
   </div>
