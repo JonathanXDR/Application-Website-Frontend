d16a51a00a8d043fcb73a47e7efaea526126caa8
Jonathan Russ <jonathan.russ@bluewin.ch>
2022-10-30 19:59:48 +0100

    Update TimeLine component

--- src/components/common/TimeLine/TimeLine.ts
+++ src/components/common/TimeLine/TimeLine.ts
@@ -13,7 +13,6 @@ import LinkCollection from '@/components/common/LinkCollection/LinkCollection.vu
 import ShareSheet from '@/components/common/ShareSheet/ShareSheet.vue';
 import CardTile from '@/components/common/CardTile/CardTile.vue';
 import ArticleItem from '@/components/common/ArticleItem/ArticleItem.vue';
-import TimeLine from '@/components/common/TimeLine/TimeLine.vue';
 
 import json from '@/assets/data/data.json';
 
@@ -32,105 +31,43 @@ export default defineComponent({
     ShareSheet,
     CardTile,
     ArticleItem,
-    TimeLine,
   },
   data() {
     return {
       data: json.components[1].data[5].data,
+      totalLength: 0,
+      scrollPosition: 0,
+      strokeDashoffset: 0,
     };
   },
   mounted() {
-    let ticking = false;
-    let last_known_scroll_position = 0;
-    let updatePath = false;
-
     const element = this.$refs['svg-timeline'] as SVGElement;
     const path = this.$refs['path-timeline'] as SVGPathElement;
-    let totalLength = 0;
+    this.totalLength = this.$parent.$refs['ul-timeline'].offsetHeight;
 
-    setTimeout(() => {
-      const timelineHeight = this.$refs['ul-timeline'].offsetHeight;
-      // console.log(Math.ceil(timelineHeight));
-      const size = Math.ceil(timelineHeight);
-      // console.log(size);
+    console.log('path', path.getTotalLength());
 
-      element.setAttribute('viewBox', `0 0 8 ${size}`);
-      element.setAttribute('height', `${size}`);
-      element.setAttribute('xmlns', `http://www.w3.org/${size}/svg`);
-
-      path.setAttribute('d', `M 4 0 L 4 ${size}`);
-
-      path.setAttribute('stroke-dasharray', `${totalLength}`);
-
-      initPath(path);
-
-      function initPath(path: SVGPathElement) {
-        totalLength = path.getTotalLength();
-        path.style.strokeDasharray = `${totalLength}`;
-        path.style.strokeDashoffset = `${totalLength}`;
-      }
-
-      function handleEntries(entries: any) {
-        console.log(entries);
-        entries.forEach((entry: any) => {
-          console.log(entry);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            console.log(entry.target);
+            // console.log("I'm visible");
+            window.addEventListener('scroll', this.handleScroll());
+          } else {
+            // console.log("I'm not visible");
+            window.removeEventListener('scroll', this.handleScroll());
           }
         });
-      }
+      },
+      { rootMargin: '-52px 0px 0px 0px' }
+    );
 
-      const observer = new IntersectionObserver(
-        (entries, observer) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              // console.log(entry);
-              updatePath = true;
-            } else {
-              updatePath = false;
-            }
-          });
-        },
-        { rootMargin: '0px 0px 0px 0px' }
-      );
-
-      observer.observe(element);
-
-      function doSomething(scroll_pos) {
-        if (!updatePath) {
-          return;
-        }
-        window.requestAnimationFrame(() => {
-          const center = window.innerHeight / 2;
-          const boundaries = path.getBoundingClientRect();
-          const top = boundaries.top;
-          const height = boundaries.height;
-          const percentage = (center - top) / height;
-          const drawLength = percentage > 0 ? totalLength * percentage : 0;
-          path.style.strokeDashoffset = `${
-            drawLength < totalLength ? totalLength - drawLength : 0
-          }`;
-        });
-      }
-
-      window.addEventListener('scroll', function (e) {
-        last_known_scroll_position = window.scrollY;
-
-        if (!ticking) {
-          window.requestAnimationFrame(function () {
-            doSomething(last_known_scroll_position);
-            ticking = false;
-          });
-
-          ticking = true;
-        }
-      });
-    }, 500);
+    observer.observe(element);
   },
-
-  // if (document.getElementById('timeline') === true) {
-  //   const timelineHeight =
-  //     document.getElementById('timeline-id').offsetHeight;
-  //   console.log(timelineHeight);
-  // }
+  methods: {
+    handleScroll() {
+      this.scrollPosition = window.scrollY;
+      console.log('scrollPosition', this.scrollPosition);
+    },
+  },
 });
--- src/components/common/TimeLine/TimeLine.vue
+++ src/components/common/TimeLine/TimeLine.vue
@@ -1,8 +1,18 @@
 <template>
-  <svg id="svg-timeline" preserveAspectRatio="none">
+  <svg
+    id="svg-timeline"
+    ref="svg-timeline"
+    preserveAspectRatio="none"
+    :height="totalLength"
+  >
     <path
+      :style="{
+        strokeDashoffset: totalLength,
+        strokeDasharray: scrollPosition,
+      }"
+      ref="path-timeline"
       class="path-timeline"
-      d="M 4 0 L 4 2250"
+      :d="`M 4 0 L 4 ${totalLength}`"
       stroke-width="5"
       fill="none"
       fill-rule="evenodd"
