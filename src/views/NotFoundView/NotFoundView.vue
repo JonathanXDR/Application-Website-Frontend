f7d2e1cc3b8636b06555dd9341cddd44f7889e33
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-02-07 19:50:32 +0100

    Update NotFoundView

--- src/views/NotFoundView/NotFoundView.vue
+++ src/views/NotFoundView/NotFoundView.vue
@@ -1,30 +1,45 @@
 <template>
-  <div id="err404" class="wrapper">
-    <div class="static-container">
-      <h1 class="title">
-        <span class="params">new</span> Error(<span class="params">404</span>)
-      </h1>
-
-      <TextBlock>
-        <p>The page you requested could not be found.</p>
-        <router-link to="/" title="Back home" class="bt">
-          <svg class="nav-ico -prev" viewBox="0 0 16 16">
-            <path d="M5,1h10v10 M5.5,10.5l0.8-0.8" />
-          </svg>
-          Back to home
-        </router-link>
-      </TextBlock>
-    </div>
-
-    <Astronaut isPlaying />
-    <CoffeeMug isPlaying />
+  <div v-for="index in 40" :key="index">
+    <span
+      class="particle"
+      :style="{
+        // top: `${Math.floor(Math.random() * 100)}%`,
+        // left: `${Math.floor(Math.random() * 100)}%`,
+        // fontSize: `${Math.floor(Math.random() * 40 + 10)}px`,
+        // filter: `blur(${Math.random() * 0.4}px)`,
+        animation: `${Math.floor(Math.random() * 40 + 10)}s ${
+          ['apparition', 'float', 'floatReverse', 'float2', 'floatReverse2'][
+            Math.floor(Math.random() * 5)
+          ]
+        } infinite`,
+      }"
+      >4</span
+    >
+    <span
+      class="particle"
+      :style="{
+        // top: `${Math.floor(Math.random() * 100)}%`,
+        // left: `${Math.floor(Math.random() * 100)}%`,
+        // fontSize: `${Math.floor(Math.random() * 40 + 10)}px`,
+        // filter: `blur(${Math.random() * 0.4}px)`,
+        // output is wrong: animation: 34s ease 0s infinite normal none running floatReverse;
+        // should be: animation: 34s floatReverse infinite;
+        // animation (output wrong): `${Math.floor(Math.random() * 40 + 10)}s ${
+        //   ['apparition', 'float', 'floatReverse', 'float2', 'floatReverse2'][
+        //     Math.floor(Math.random() * 5)
+        //   ]
+        // } infinite`,
+      }"
+      >0</span
+    >
   </div>
+  <article class="content">
+    <p>Damnit stranger,</p>
+    <p>You got lost in the <strong>404</strong> galaxy.</p>
+    <p>
+      <button>Go back to earth.</button>
+    </p>
+  </article>
 </template>
 
-<script lang="ts">
-export default {
-  name: "NotFound",
-};
-</script>
-
-<style scoped></style>
+<style scoped src="./NotFoundView.css"></style>
