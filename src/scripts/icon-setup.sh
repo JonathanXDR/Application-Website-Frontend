1fa8d6f9c55b11ae6d25b4169fb65b19f70a29a0
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-12 16:06:22 +0200

    Update icon-setup.sh

--- src/scripts/icon-setup.sh
+++ src/scripts/icon-setup.sh
@@ -19,6 +19,17 @@ for file in *.svg; do
     sed -i '' '/^<!--Generator: Apple Native CoreSVG 232.5-->/d' "$file"
     sed -i '' '/^<!DOCTYPE svg/,/^       "http:\/\/www.w3.org\/Graphics\/SVG\/1.1\/DTD\/svg11.dtd">/d' "$file"
 
+    # Extract the width and height attributes
+    width=$(xmlstarlet sel -t -v "/svg/@width" "$file")
+    height=$(xmlstarlet sel -t -v "/svg/@height" "$file")
+
+    # Use xmlstarlet to update the fill attribute, remove the fill-opacity attribute, and set the viewBox attribute
+    xmlstarlet ed -L \
+        -u "//@fill" -v "currentColor" \
+        -d "//@fill-opacity" \
+        -i "/svg" -t attr -n "viewBox" -v "0 0 $width $height" \
+        "$file"
+
     # Format the SVG file with Prettier
     prettier --write "$file"
 done
