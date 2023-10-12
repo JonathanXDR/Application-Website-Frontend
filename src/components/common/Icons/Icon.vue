cce891bd1c608dbebd1aa3593d1bb1cef832efd3
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-12 21:55:31 +0200

    Update Icon component

--- src/components/common/Icons/Icon.vue
+++ src/components/common/Icons/Icon.vue
@@ -1,9 +1,43 @@
 <template>
-  <div v-html="icon" />
+  <div
+    v-svg="{
+      path: `src/assets/icons/${size}/${name}.svg`,
+      attrs: $attrs
+    }"
+  ></div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+const svgDirective = {
+  async mounted(el, binding) {
+    const { path, attrs } = binding.value
+    const response = await fetch(path)
+    if (response.ok) {
+      const text = await response.text()
+      const tempContainer = document.createElement('div')
+      tempContainer.innerHTML = text
+      const svgElement = tempContainer.firstChild as Element
+      for (const [key, value] of Object.entries(attrs)) {
+        if (key === 'style' && value && typeof value === 'object') {
+          Object.assign(svgElement.style, value)
+        } else {
+          svgElement.setAttribute(key, value as string)
+        }
+      }
+      el.parentNode.replaceChild(svgElement, el)
+    } else {
+      console.error(`Failed to load SVG: ${response.statusText}`)
+    }
+  }
+}
+
+export default defineComponent({
+  inheritAttrs: false,
+  directives: {
+    svg: svgDirective
+  },
   props: {
     name: {
       type: String,
@@ -12,32 +46,8 @@ export default {
     size: {
       type: String,
       default: 'medium',
-      validator: (value) => ['big', 'medium', 'small'].includes(value)
-    }
-  },
-  data() {
-    return {
-      icon: ''
-    }
-  },
-  mounted() {
-    this.loadIcon()
-  },
-  methods: {
-    async loadIcon() {
-      const iconPath = `src/assets/icons/${this.size}/${this.name}.svg`
-      try {
-        const response = await fetch(iconPath)
-        if (!response.ok) {
-          console.error(`Failed to load icon: ${response.statusText}`)
-          return
-        }
-        const text = await response.text()
-        this.icon = text
-      } catch (error) {
-        console.error(`Failed to load icon: ${error.message}`)
-      }
+      validator: (value: string) => ['big', 'medium', 'small'].includes(value)
     }
   }
-}
+})
 </script>
