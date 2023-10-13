70ff403f0124cd821035315f32ad70827d716d94
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-13 21:15:25 +0200

    Cleanup FilterNav component

--- src/components/common/FilterNav/FilterNav.vue
+++ src/components/common/FilterNav/FilterNav.vue
@@ -2,7 +2,7 @@
   <fieldset class="sticky-nav tile tile-rounded sticking">
     <legend class="visuallyhidden">Data filter by job type</legend>
     <div class="form-dropdown">
-      <select class="form-dropdown-select" name="year" aria-label="Year">
+      <select class="form-dropdown-select" name="year">
         <option value="2022">2022</option>
         <option value="2021">2021</option>
         <option value="2020">2020</option>
@@ -13,35 +13,33 @@
         <option value="2015">2015</option>
         <option value="2014">2014</option>
       </select>
-      <span class="form-dropdown-chevron" aria-hidden="true"></span>
+      <span class="form-dropdown-chevron"></span>
     </div>
     <div class="role-selects-container">
       <div class="role-selects">
         <div class="role-select">
           <input type="radio" name="role" value="overall" checked />
-          <span id="role-overall-label" aria-hidden="true" class="button">Overall</span>
+          <span id="role-overall-label" class="button">Overall</span>
         </div>
         <div class="role-select">
           <input type="radio" name="role" value="tech" />
-          <span id="role-tech-label" aria-hidden="true" class="button">Tech</span>
+          <span id="role-tech-label" class="button">Tech</span>
         </div>
         <div class="role-select">
           <input type="radio" name="role" value="nontech" />
-          <span id="role-nontech-label" aria-hidden="true" class="button">Nontech</span>
+          <span id="role-nontech-label" class="button">Nontech</span>
         </div>
         <div class="role-select">
           <input type="radio" name="role" value="leadership" />
-          <span id="role-leadership-label" aria-hidden="true" class="button">Leadership</span>
+          <span id="role-leadership-label" class="button">Leadership</span>
         </div>
         <div class="role-select">
           <input type="radio" name="role" value="retail" />
-          <span id="role-retail-label" aria-hidden="true" class="button">Retail</span>
+          <span id="role-retail-label" class="button">Retail</span>
         </div>
         <div class="role-select">
           <input type="radio" name="role" value="retailLeadership" />
-          <span id="role-retailLeadership-label" aria-hidden="true" class="button"
-            >Retail Leadership</span
-          >
+          <span id="role-retailLeadership-label" class="button">Retail Leadership</span>
         </div>
       </div>
     </div>
