83b071c750a7ea00c51c242c6a55febacb3b6046
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-13 18:23:43 +0200

    Add MusicSection

---
+++ src/components/containers/MusicSection/MusicSection.ts
@@ -0,0 +1,31 @@
+import LoadingSpinner from '@/components/common/LoadingSpinner/LoadingSpinner.vue'
+import { fetchData } from '@/helpers/locale-helper'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MusicSection',
+  components: {
+    LoadingSpinner
+  },
+  data() {
+    return {
+      json: undefined as any
+    }
+  },
+  watch: {
+    '$i18n.locale': 'fetchLocalizedData'
+  },
+  methods: {
+    async fetchLocalizedData() {
+      try {
+        const data = (await fetchData()) as any
+        this.json = data.components.containers.languages
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+  },
+  created() {
+    this.fetchLocalizedData()
+  }
+})
---
+++ src/components/containers/MusicSection/MusicSection.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2>{{ $t('components.containers.common.music') }}</h2>
+  <LoadingSpinner v-else />
+</template>
+
+<script lang="ts" src="./MusicSection.ts"></script>
+<style scoped src="./MusicSection.css"></style>
