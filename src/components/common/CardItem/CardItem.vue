8db2a943e62ba4886047423e9636ab07e2ce1f47
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-14 20:10:54 +0200

    Update CardItem

--- src/components/common/CardItem/CardItem.vue
+++ src/components/common/CardItem/CardItem.vue
@@ -3,8 +3,17 @@
     href="https://docs.swift.org/swift-book/documentation/the-swift-programming-language/macros"
     class="card large"
   >
-    <div class="card-cover-wrap">
-      <picture class="card-cover"
+    <div
+      class="card-cover-wrap"
+      style="
+        display: flex;
+        flex-direction: column;
+        color: #1d1d1f;
+        background: #f5f5f7;
+        padding-inline: 17px;
+      "
+    >
+      <!-- <picture class="card-cover"
         ><source
           media="(prefers-color-scheme: dark)"
           srcset="
@@ -22,7 +31,43 @@
           width="317"
           height="auto"
         />
-      </picture>
+      </picture> -->
+      <div
+        class="group group-hispanic typography-body-tight"
+        style="
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        "
+      >
+        <p>
+          <span class="semibold"> <span data-value="">27.1</span>% </span>
+          <span>Hispanic/Latinx</span>
+        </p>
+        <div class="bar-container" style="width: 100%">
+          <div class="bar" data-bar="" style="width: 27.1%"></div>
+        </div>
+      </div>
+      <div
+        class="group group-white typography-body-tight"
+        style="
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        "
+      >
+        <p>
+          <span class="semibold"> <span data-value="">39.7</span>% </span>
+          <span>White</span>
+        </p>
+        <div class="bar-container" style="width: 100%">
+          <div class="bar" data-bar="" style="width: 39.7%"></div>
+        </div>
+      </div>
     </div>
     <div class="details">
       <div id="card_eyebrow_98" class="eyebrow">Language feature</div>
@@ -32,13 +77,18 @@
           Learn how Swift macros can make your code cleaner and more powerful.
         </div>
       </div>
-      <div class="link">
-        Read documentation
-        <Icon
-          name="chevron.right"
-          size="small"
-          class="svg-icon inline-chevron-right-icon icon-inline link-icon"
-        />
+      <div class="ctas-wrapper">
+        <a href="photos://" class="icon-wrapper button button-reduced button-neutral">
+          <span class="icon-copy"> Open</span>
+        </a>
+        <button class="icon-wrapper secondary-cta link typography-body-reduced modal-trigger">
+          <span class="icon-copy">Learn more</span>
+          <Icon
+            name="chevron.right"
+            size="small"
+            class="svg-icon inline-chevron-right-icon icon-inline link-icon"
+          />
+        </button>
       </div>
     </div>
   </a>
