b35393ec1c5bdef16221fb5da5f43270d767957c
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-14 20:30:56 +0200

    Update CardItem

--- src/components/common/CardItem/CardItem.vue
+++ src/components/common/CardItem/CardItem.vue
@@ -2,8 +2,9 @@
   <a
     href="https://docs.swift.org/swift-book/documentation/the-swift-programming-language/macros"
     class="card large"
+    style="border-radius: 30px !important"
   >
-    <div class="card-cover-wrap" style="color: #1d1d1f; background: #f5f5f7; padding: 17px">
+    <div class="card-cover-wrap" style="color: #1d1d1f; background: #f5f5f7; padding: 30px">
       <!-- <picture class="card-cover"
         ><source
           media="(prefers-color-scheme: dark)"
@@ -23,26 +24,62 @@
           height="auto"
         />
       </picture> -->
-      <div class="group group-hispanic typography-body-tight">
+      <!-- <div class="group group-hispanic typography-body-tight">
         <p>
-          <span class="semibold"> <span data-value="">27.1</span>% </span>
+          <span class="semibold"> <span data-value>27.1</span>% </span>
           <span>Hispanic/Latinx</span>
         </p>
         <div class="bar-container">
-          <div class="bar" data-bar="" style="width: 27.1%"></div>
+          <div class="bar" data-bar style="width: 27.1%"></div>
         </div>
       </div>
       <div class="group group-white typography-body-tight">
         <p>
-          <span class="semibold"> <span data-value="">39.7</span>% </span>
+          <span class="semibold"> <span data-value>39.7</span>% </span>
           <span>White</span>
         </p>
         <div class="bar-container">
-          <div class="bar" data-bar="" style="width: 39.7%"></div>
+          <div class="bar" data-bar style="width: 39.7%"></div>
+        </div>
+      </div> -->
+      <div class="container">
+        <div class="donut-container">
+          <figure class="donut">
+            <svg class="ac-graph-svg ac-graph-donut">
+              <path
+                class="ac-graph-path donut-wedge wedge-1"
+                d="M 113.99911851558227 0.07271925905595822 A 110 110 0 0 1 106.00088148441777 219.92728074094404 A 110 110 0 0 1 25.14997985191927 180.00338621002766 L 32.09225422767132 174.27583642920723 A 101 101 0 0 0 106.32808209023814 210.93323049850318 A 101 101 0 0 0 113.6719179097619 9.066769501496836 Z"
+              ></path>
+              <path
+                class="ac-graph-path donut-wedge wedge-2"
+                d="M 20.287609232531437 173.6528628011735 A 110 110 0 0 1 105.31027149295572 0.10001616683369718 L 105.69397655262298 9.09183302591093 A 101 101 0 0 0 27.62771393168795 168.44490129925933 Z"
+              ></path>
+            </svg>
+          </figure>
+          <h4 class="sector-labels typography-donut-label">
+            <span>Global</span>
+            <!-- <span>United States, Canada, Latin America, and the Caribbean</span>
+            <span>Asia Pacific</span>
+            <span>Europe, Middle East, <br class="large medium" />India, and Africa</span> -->
+          </h4>
+        </div>
+        <div class="wedge-legend typography-donut-label female">
+          <span>
+            <span class="semibold">Female</span>
+            <br />
+            <span data-value>35.3</span>%
+          </span>
+        </div>
+        <div class="wedge-legend typography-donut-label male">
+          <span>
+            <span class="semibold">Male</span>
+            <br />
+            <span data-value>64.6</span>%
+          </span>
         </div>
       </div>
     </div>
-    <div class="details">
+    <div class="details" style="padding: 30px !important">
       <div id="card_eyebrow_98" class="eyebrow">Language feature</div>
       <div id="card_title_98" class="title">Macros</div>
       <div id="card_content_98" class="card-content">
