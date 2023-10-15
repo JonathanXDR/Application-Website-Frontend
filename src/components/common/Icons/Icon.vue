135e7c5e707943fcef0007297c6ffef5b50ca259
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-15 16:04:20 +0200

    Update Icon

--- src/components/common/Icons/Icon.vue
+++ src/components/common/Icons/Icon.vue
@@ -1,13 +1,10 @@
 <template>
   <svg>
-    <use :xlink:href="iconHref" />
+    <use :href="icon" />
   </svg>
 </template>
 
 <script lang="ts">
-import large from '@/assets/icons/large/symbol/sprite.svg'
-import medium from '@/assets/icons/medium/symbol/sprite.svg'
-import small from '@/assets/icons/small/symbol/sprite.svg'
 import { computed, defineComponent } from 'vue'
 
 export default defineComponent({
@@ -24,20 +21,12 @@ export default defineComponent({
     }
   },
   setup(props) {
-    const iconHref = computed(() => {
-      switch (props.size) {
-        case 'small':
-          return `${small}#${props.name}`
-        case 'medium':
-          return `${medium}#${props.name}`
-        case 'large':
-          return `${large}#${props.name}`
-        default:
-          return `${medium}#${props.name}`
-      }
-    })
+    const getSpriteUrl = (size) => {
+      return new URL(`/src/assets/icons/${size}/symbol/sprite.svg`, import.meta.url).href
+    }
+    const icon = computed(() => `${getSpriteUrl(props.size)}#${props.name}`)
 
-    return { iconHref }
+    return { icon }
   }
 })
 </script>
