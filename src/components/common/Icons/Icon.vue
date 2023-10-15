77f264556310e94c783acbf3d6096d28d42ade94
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-15 15:48:34 +0200

    Update Icon

--- src/components/common/Icons/Icon.vue
+++ src/components/common/Icons/Icon.vue
@@ -1,13 +1,17 @@
 <template>
-  <div v-html="icon"></div>
+  <svg>
+    <use :xlink:href="iconHref" />
+  </svg>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue'
+import large from '@/assets/icons/large/symbol/sprite.svg'
+import medium from '@/assets/icons/medium/symbol/sprite.svg'
+import small from '@/assets/icons/small/symbol/sprite.svg'
+import { computed, defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'SvgComponent',
-  inheritAttrs: false,
   props: {
     name: {
       type: String,
@@ -20,64 +24,20 @@ export default defineComponent({
     }
   },
   setup(props) {
-    const icon = ref('')
-
-    // const getImageUrl = (size: string, name: string): string => {
-    //   return new URL(`@/assets/icons/${size}/${name}.svg`, import.meta.url).href
-    // }
-
-    const importIcon = async (name: string, size: string) => {
-      return import(`/src/assets/icons/${size}/${name}.svg`)
-    }
-
-    const fetchIcon = async () => {
-      const module = await importIcon(props.name, props.size)
-      const url = module.default
-      const response = await fetch(url)
-      if (response.ok) {
-        const text = await response.text()
-        icon.value = text
-      } else {
-        console.error('Failed to fetch icon:', response.status, response.statusText)
+    const iconHref = computed(() => {
+      switch (props.size) {
+        case 'small':
+          return `${small}#${props.name}`
+        case 'medium':
+          return `${medium}#${props.name}`
+        case 'large':
+          return `${large}#${props.name}`
+        default:
+          return `${medium}#${props.name}`
       }
-    }
+    })
 
-    onMounted(fetchIcon)
-
-    return { icon }
+    return { iconHref }
   }
 })
 </script>
-
-<style>
-#references .icon-article {
-  width: 1.25em;
-  height: 1.25em;
-}
-
-#projects .icon-article {
-  width: 1.5em;
-  height: 1.5em;
-}
-
-.link .link-icon {
-  /* height: 0.6em;
-  width: 0.6em; */
-  height: 0.75em;
-  width: 0.75em;
-  margin-left: 0.3em;
-}
-
-.svg-icon {
-  fill: var(--colors-svg-icon-fill-light, var(--color-svg-icon));
-  transform: scale(1);
-  -webkit-transform: scale(1);
-  overflow: visible;
-}
-
-.svg-icon.icon-inline {
-  display: inline-block;
-  vertical-align: middle;
-  fill: currentColor;
-}
-</style>
