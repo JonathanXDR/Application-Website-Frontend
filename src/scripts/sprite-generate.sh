5cc43eac69b4abf38be4ca5473e01ec66c259a61
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-15 14:58:49 +0200

    Add script for generating sprites

---
+++ src/scripts/sprite-generate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+dir="../assets/icons"
+abs_dir=$(cd "$(dirname "$0")" && cd "$dir" && pwd)
+
+if [[ ! -d "$abs_dir" ]]; then
+  echo "Directory '$abs_dir' does not exist."
+  exit 1
+fi
+
+generate_sprite() {
+  local sub_dir=$1
+  echo "Generating sprite for $sub_dir..."
+  cd "$abs_dir/$sub_dir" || {
+    echo "Failed to navigate to $abs_dir/$sub_dir"
+    exit 1
+  }
+  svg-sprite --symbol --symbol-sprite=sprite.svg ./*.svg
+  echo "Sprite generated for $sub_dir."
+}
+
+for sub_dir in small medium large; do
+  generate_sprite "$sub_dir"
+done
+
+# Return to the original directory
+cd - >/dev/null
