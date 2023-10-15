b4636815480056c710b3f9468fc14856b8fa31d3
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-15 02:51:44 +0200

    Fix errors

--- src/components/common/ArticleItem/ArticleItem.ts
+++ src/components/common/ArticleItem/ArticleItem.ts
@@ -1,6 +1,6 @@
 import Icon from '@/components/common/Icons/Icon.vue'
 import LinkCollection from '@/components/common/LinkCollection/LinkCollection.vue'
-import type { ArticleItem } from '@/types/common/ArticleItem'
+import type { ArticleItemType } from '@/types/common/ArticleItem'
 import { defineComponent, type PropType } from 'vue'
 
 export default defineComponent({
@@ -11,7 +11,7 @@ export default defineComponent({
   },
   props: {
     article: {
-      type: Object as PropType<ArticleItem>,
+      type: Object as PropType<ArticleItemType>,
       required: true,
       default: () => ({})
     }
--- src/components/common/CardTile/CardTile.ts
+++ src/components/common/CardTile/CardTile.ts
@@ -1,11 +1,11 @@
-import type { CardTile } from '@/types/common/CardTile'
+import type { CardTileType } from '@/types/common/CardTile'
 import { defineComponent, ref, type PropType } from 'vue'
 
 export default defineComponent({
   name: 'CardTile',
   props: {
     card: {
-      type: Object as PropType<CardTile>,
+      type: Object as PropType<CardTileType>,
       required: true,
       default: () => ({})
     }
