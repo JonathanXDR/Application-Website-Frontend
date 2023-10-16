c0696b9a806e8007ca406f75ddfb34d7f98c5580
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-16 17:17:41 +0200

    Update RibbonBar

--- src/components/common/RibbonBar/RibbonBar.ts
+++ src/components/common/RibbonBar/RibbonBar.ts
@@ -2,7 +2,7 @@ import LinkCollection from '@/components/common/LinkCollection/LinkCollection.vu
 import LoadingSpinner from '@/components/common/LoadingSpinner/LoadingSpinner.vue'
 import { listRepositoryTags } from '@/helpers/github-helper'
 import type { LinkType } from '@/types/common/Link'
-import { defineComponent, onMounted, ref } from 'vue'
+import { computed, defineComponent, onMounted, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
@@ -12,8 +12,7 @@ export default defineComponent({
     LinkCollection
   },
   setup() {
-    const { tm } = useI18n()
-    const links = ref<LinkType[]>(tm('components.common.RibbonBar.links'))
+    const { tm, t } = useI18n()
     const tags = ref<{ latest: string | undefined; previous: string | undefined }>({
       latest: undefined,
       previous: undefined
@@ -29,8 +28,17 @@ export default defineComponent({
       tags.value = { latest: latest.name, previous: previous.name }
     }
 
+    const links = computed(() => {
+      return (tm('components.common.RibbonBar.links') as LinkType[]).map((link) => {
+        return {
+          ...link,
+          url: t(link.url, { latestTag: tags.value.latest, previousTag: tags.value.previous })
+        }
+      })
+    })
+
     onMounted(fetchTags)
 
-    return { tm, links, tags }
+    return { links, tags }
   }
 })
