9caeba37f6cb328e1e7024eda1f2aa1d685da916
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-16 18:28:42 +0200

    Add MaintenanceVideoView

---
+++ src/views/MaintenanceVideoView/MaintenanceView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="rs-covers rs-covers-preorder">
+    <div class="rs-covers-transtion theme-dark r-fade-transition-enter-done">
+      <div
+        class="rs-covers-container rs-covers-media-default rs-covers-textovervideo as-l-container"
+      >
+        <div class="rs-covers-media-container">
+          <div
+            id="rs-covers-media"
+            class="rc-videoplayer rs-covers-media-wrapper rc-videoplayer-muted rc-videoplayer-stalled rc-videoplayer-medium"
+          >
+            <video
+              class="rc-video rs-covers-media"
+              preload="metadata"
+              loop
+              autoplay
+              playsinline
+              aria-label="From red to violet, a spectrum of streaming colors outlines the Apple logo."
+              role="img"
+            >
+              <source
+                src="https://web.archive.org/web/20230915074251/https://store.storeimages.cdn-apple.com/2/store.apple.com/shop/rs-covers/2/dist/media/app-covers-apple-outline.mp4"
+                type="video/mp4"
+              />
+              <p>Video is not supported</p>
+            </video>
+            <div class="rc-videoplayer-poster">
+              <img
+                alt=""
+                src="https://web.archive.org/web/20230915074251/https://store.storeimages.cdn-apple.com/2/store.apple.com/shop/rs-covers/2/dist/images/covers-apple-outline.jpg"
+              />
+            </div>
+          </div>
+          <button type="button" class="rs-covers-mediabtn icon icon-after icon-thumbnailplay">
+            <span class="visuallyhidden">play animation</span>
+          </button>
+        </div>
+        <div class="rs-covers-content-container">
+          <h1 class="rs-covers-heading">
+            <span>We can't wait either.</span>
+          </h1>
+          <div class="rs-covers-desc">
+            Pre-order begins at 5:00 a.m. PDT. <span class="nowrap">See you then.</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="theme-dark rs-covers-footer">
+      <div class="as-l-container">Copyright © 2023 Apple Inc. All rights reserved.</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./MaintenanceView.ts"></script>
+<style scoped src="./MaintenanceView.css"></style>
