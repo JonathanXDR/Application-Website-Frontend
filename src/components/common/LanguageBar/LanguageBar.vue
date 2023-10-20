b4ea747656b7dc63989284ecf1aa38fd32fc5dc0
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-21 01:01:43 +0200

    Update animation directive

--- src/components/common/LanguageBar/LanguageBar.vue
+++ src/components/common/LanguageBar/LanguageBar.vue
@@ -1,12 +1,12 @@
 <template>
   <li class="graph-container">
     <div :style="'width: ' + language.progress + '%'">
-      <div v-animation class="graph-bar current"></div>
-      <p v-animation class="graph-caption typography-graph-caption current">
+      <div v-animation="'visible'" class="graph-bar current"></div>
+      <p v-animation="'visible'" class="graph-caption typography-graph-caption current">
         {{ language.description }}
       </p>
     </div>
-    <div v-animation class="typography-eyebrow-elevated secondary-badge-value current">
+    <div v-animation="'visible'" class="typography-eyebrow-elevated secondary-badge-value current">
       {{ language.progress }}%
     </div>
   </li>
