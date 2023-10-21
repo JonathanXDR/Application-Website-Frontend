a1483680f248cdc7369626146b1bf9cbfdc2c4c7
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-21 19:41:38 +0200

    Update TabList

--- src/components/common/TabList/TabList.ts
+++ src/components/common/TabList/TabList.ts
@@ -1,9 +1,21 @@
 import Icon from '@/components/common/Icons/Icon.vue'
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 
 export default defineComponent({
   name: 'TabList',
   components: {
     Icon
+  },
+  setup() {
+    const items = reactive([
+      { id: 'productivity', label: 'Productivity' },
+      { id: 'gaming', label: 'Gaming' },
+      { id: 'production', label: 'Audio and video production' },
+      { id: 'development', label: 'Software development' }
+    ])
+
+    return {
+      items
+    }
   }
 })
--- src/components/common/TabList/TabList.vue
+++ src/components/common/TabList/TabList.vue
@@ -2,21 +2,9 @@
   <div class="tablist-wrapper">
     <div class="tabnav">
       <ul role="tablist" class="tabnav-items">
-        <li role="presentation" class="tabnav-item current">
-          <input id="productivity" type="radio" name="category" checked />
-          <label for="productivity" class="tabnav-link current">Productivity</label>
-        </li>
-        <li role="presentation" class="tabnav-item">
-          <input id="gaming" type="radio" name="category" />
-          <label for="gaming" class="tabnav-link">Gaming</label>
-        </li>
-        <li role="presentation" class="tabnav-item">
-          <input id="production" type="radio" name="category" />
-          <label for="production" class="tabnav-link">Audio and video production</label>
-        </li>
-        <li role="presentation" class="tabnav-item">
-          <input id="development" type="radio" name="category" />
-          <label for="development" class="tabnav-link">Software development</label>
+        <li v-for="(item, index) in items" :key="index" class="tabnav-item">
+          <input :id="item.id" type="radio" name="category" :value="index" />
+          <label :for="item.id" class="tabnav-link">{{ item.label }}</label>
         </li>
       </ul>
       <div class="tabnav-paddles">
