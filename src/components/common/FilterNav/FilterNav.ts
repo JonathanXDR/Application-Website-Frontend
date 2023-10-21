e1a57bf526d6814c906d69ce24c50628e4b4e8ad
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-21 19:38:22 +0200

    Update FilterNav

--- src/components/common/FilterNav/FilterNav.ts
+++ src/components/common/FilterNav/FilterNav.ts
@@ -1,9 +1,36 @@
 import Icon from '@/components/common/Icons/Icon.vue'
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 
 export default defineComponent({
   name: 'FilterNav',
   components: {
     Icon
+  },
+  setup() {
+    const items = reactive([
+      { id: 'overall', label: 'Overall' },
+      { id: 'tech', label: 'Tech' },
+      { id: 'nontech', label: 'Non-Tech' },
+      { id: 'leadership', label: 'Leadership' },
+      { id: 'retail', label: 'Retail' },
+      { id: 'retailLeadership', label: 'Retail Leadership' }
+    ])
+
+    const options = reactive([
+      { id: '2022', label: '2022' },
+      { id: '2021', label: '2021' },
+      { id: '2020', label: '2020' },
+      { id: '2019', label: '2019' },
+      { id: '2018', label: '2018' },
+      { id: '2017', label: '2017' },
+      { id: '2016', label: '2016' },
+      { id: '2015', label: '2015' },
+      { id: '2014', label: '2014' }
+    ])
+
+    return {
+      items,
+      options
+    }
   }
 })
--- src/components/common/FilterNav/FilterNav.vue
+++ src/components/common/FilterNav/FilterNav.vue
@@ -3,15 +3,9 @@
     <legend class="visuallyhidden">Data filter by job type</legend>
     <div class="form-dropdown">
       <select class="form-dropdown-select" name="year">
-        <option value="2022">2022</option>
-        <option value="2021">2021</option>
-        <option value="2020">2020</option>
-        <option value="2019">2019</option>
-        <option value="2018">2018</option>
-        <option value="2017">2017</option>
-        <option value="2016">2016</option>
-        <option value="2015">2015</option>
-        <option value="2014">2014</option>
+        <option v-for="(option, index) in options" :key="index" :value="index">
+          {{ option.label }}
+        </option>
       </select>
       <span class="form-dropdown-chevron">
         <Icon name="chevron.down" size="small" />
@@ -19,29 +13,9 @@
     </div>
     <div class="role-selects-container">
       <ul class="role-selects">
-        <li class="role-select">
-          <input id="overall" type="radio" name="category" />
-          <label for="overall" class="button">Overall</label>
-        </li>
-        <li class="role-select">
-          <input id="tech" type="radio" name="category" />
-          <label for="tech" class="button">Tech</label>
-        </li>
-        <li class="role-select">
-          <input id="nontech" type="radio" name="category" />
-          <label for="nontech" class="button">Nontech</label>
-        </li>
-        <li class="role-select">
-          <input id="leadership" type="radio" name="category" />
-          <label for="leadership" class="button">Leadership</label>
-        </li>
-        <li class="role-select">
-          <input id="retail" type="radio" name="category" />
-          <label for="retail" class="button">Retail</label>
-        </li>
-        <li class="role-select">
-          <input id="retailLeadership" type="radio" name="category" />
-          <label for="retailLeadership" class="button">Retail Leadership</label>
+        <li v-for="(item, index) in items" :key="index" class="role-select">
+          <input :id="item.id" type="radio" name="category" :value="index" />
+          <label :for="item.id" class="button">{{ item.label }}</label>
         </li>
       </ul>
     </div>
