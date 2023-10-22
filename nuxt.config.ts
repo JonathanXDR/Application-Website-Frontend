e676840eb3e4969825d0994049ab2e152c82f756
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-22 13:53:19 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
+import { defaultLocale, fallbackLocale, languages } from "./src/assets/lang";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  plugins: [
+    { src: '~/plugins/font-awesome.ts' },
+  ],
+  modules: [
+    [
+      '@nuxtjs/i18n',
+      {
+        locales: languages,
+        defaultLocale: defaultLocale,
+        vueI18n: {
+          fallbackLocale: fallbackLocale,
+          messages: languages
+        }
+      }
+    ]
+  ]
 })
