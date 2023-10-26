1cd9c80b5bdddeb9203e992d789f2af04de53b98
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-26 10:25:46 +0200

    Update ButtonItem

--- src/components/common/ButtonItem/ButtonItem.vue
+++ src/components/common/ButtonItem/ButtonItem.vue
@@ -1,4 +1,8 @@
-<template></template>
+<template>
+  <component :is="as">
+    <slot />
+  </component>
+</template>
 
 <script lang="ts" src="./ButtonItem.ts"></script>
 <style scoped src="./ButtonItem.css"></style>
