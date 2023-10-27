a36784bef7cb2413ccab7049ec89ae98a83cc2d3
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-27 15:22:24 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@ import { fallbackLocale, languages, locale } from './assets/lang/index';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
   modules: ['@nuxtjs/i18n'],
   i18n: {
     locales: ['de', 'en', 'fr', 'it'],
