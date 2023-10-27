472ecefe657f7ee42016235839a932bfb588e6b5
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-27 12:34:27 +0200

    Update SegmentNav

--- src/components/common/SegmentNav/SegmentNav.ts
+++ src/components/common/SegmentNav/SegmentNav.ts
@@ -3,11 +3,6 @@ import type { OptionType } from '@/types/common/Option'
 import { computed, defineComponent, onMounted, ref, type Ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-interface Segment {
-  width: number
-  offset: number
-}
-
 export default defineComponent({
   name: 'SegmentNav',
   components: { DropDown },
@@ -22,14 +17,14 @@ export default defineComponent({
     const currentIndex: Ref<number> = ref(0)
     const isLoaded: Ref<boolean> = ref(false)
     const segmentNavEl: Ref<HTMLUListElement | null> = ref(null)
-    const segments: Ref<Map<number, Segment>> = ref(new Map())
+    const segments: Ref<Map<number, HTMLElement>> = ref(new Map())
 
     const selectionStyle = computed(() => {
       const segment = segments.value.get(currentIndex.value)
       return segment
         ? {
-            width: `${segment.width}px`,
-            transform: `translateX(${segment.offset}px)`
+            width: `${segment.offsetWidth}px`,
+            transform: `translateX(${segment.offsetLeft}px)`
           }
         : {}
     })
@@ -38,10 +33,7 @@ export default defineComponent({
       if (segmentNavEl.value) {
         const segmentNavItems = segmentNavEl.value.querySelectorAll('.segmentnav-item')
         segmentNavItems.forEach((item, index) => {
-          segments.value.set(index, {
-            width: item.offsetWidth,
-            offset: item.offsetLeft
-          })
+          segments.value.set(index, item as HTMLElement)
         })
       }
     }
