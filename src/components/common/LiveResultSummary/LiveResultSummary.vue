38c7d5fa9682e952a745da3ffc85286f508edc39
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-27 10:07:53 +0200

    Add LiveResultSummary component

---
+++ src/components/common/LiveResultSummary/LiveResultSummary.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="live-result-summary" class="results">
+    <div class="number-results">Showing 4 results</div>
+    <div class="changesnav-totals">
+      <div class="changed-total changed-total-modified">
+        <span>Modified</span><span class="total"> (4)</span>
+      </div>
+      <div class="changed-total changed-total-added">
+        <span>Added</span><span class="total"> (0)</span>
+      </div>
+      <div class="changed-total changed-total-deprecated">
+        <span>Deprecated</span><span class="total"> (0)</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./LiveResultSummary.ts"></script>
+<style scoped src="./LiveResultSummary.css"></style>
