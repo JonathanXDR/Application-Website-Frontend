a414f8ab4ed46ca563af3466cc4992af1b5a8950
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-10-27 10:08:08 +0200

    Add ResultBlankState component

---
+++ src/components/common/ResultBlankState/ResultBlankState.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="no-results">No results found Try changing or removing text and tags.</div>
+</template>
+
+<script lang="ts" src="./ResultBlankState.ts"></script>
+<style scoped src="./ResultBlankState.css"></style>
