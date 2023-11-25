829fa6e3bf394cb9240e1b313419e309cafe4714
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-11-25 19:40:42 +0100

    Add AnchorButton

---
+++ src/components/common/AnchorButton/AnchorButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="nr-cta-circular-icon nr-cta-open">
+    <Icon name="plus" size="small" />
+  </div>
+</template>
+
+<script lang="ts" src="./AnchorButton.ts"></script>
+<style scoped src="./AnchorButton.css"></style>
+./AnchorButton.js./AnchorButton.js
