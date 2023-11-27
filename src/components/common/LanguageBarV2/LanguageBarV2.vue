de593a9cec349441d3f1f8dbd5777df9840285cc
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-11-27 08:31:06 +0100

    Add LanguageBarV2

---
+++ src/components/common/LanguageBarV2/LanguageBarV2.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="column large-7 small-12 rs-od-iteminfo">
+    <h2 class="rs-od-itemtitle">
+      <span class="rs-od-itemshortname"
+        >AirPods Pro (2.&nbsp;Generation) mit MagSafe Ladecase (USB‑C)</span
+      ><span
+        class="rs-od-itemstatus"
+        tabindex="-1"
+        aria-atomic="true"
+        aria-live="polite"
+        role="timer"
+        >Arrives 29 Nov</span
+      >
+    </h2>
+    <div class="rs-od-delivery-actions"></div>
+    <div
+      id="rs-tracker-orderDetail.orderItems.orderItem-0000101.orderItemStatusTracker"
+      class="rs-tracker as-l-container-full-small as-l-container-mobileoverflow"
+    >
+      <div class="as-l-container-mobileoverflow-content">
+        <div class="rs-tracker-bar"><div class="rs-tracker-fill" style="width: 50%"></div></div>
+        <ol class="rs-tracker-status" role="list">
+          <li class="rs-status-tracker-status-list-item" style="width: 20%">
+            <span aria-current="false" class="rs-status">Order Placed</span
+            ><span class="rs-status-a11y">(Completed)</span>
+          </li>
+          <li class="rs-status-tracker-status-list-item" style="width: 20%">
+            <span aria-current="false" class="rs-status">Processing</span
+            ><span class="rs-status-a11y">(Completed)</span>
+          </li>
+          <li class="rs-status-tracker-status-list-item" style="width: 20%">
+            <span aria-current="step" class="rs-status rs-status-current">Preparing to Ship</span
+            ><span class="rs-status-a11y">(In Progress)</span>
+          </li>
+          <li class="rs-status-tracker-status-list-item" style="width: 20%">
+            <span aria-current="false" class="rs-status">Shipped</span
+            ><span class="rs-status-a11y">(Incomplete)</span>
+          </li>
+          <li class="rs-status-tracker-status-list-item" style="width: 20%">
+            <span aria-current="false" class="rs-status">Delivered</span
+            ><span class="rs-status-a11y">(Incomplete)</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+    <div class="row rs-od-itemdesc-wrapper">
+      <div class="rs-od-itemdesc column large-12 small-12">
+        <div class="rs-od-itemdesc-content">
+          When your order has shipped, we'll send you tracking information.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./LanguageBarV2.ts"></script>
+<style scoped src="./LanguageBarV2.css"></style>
