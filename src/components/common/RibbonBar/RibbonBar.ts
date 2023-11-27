26ab078aae25b418e29526642aa96d038b100003
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-11-27 11:42:25 +0100

    Update RibbonBar

--- src/components/common/RibbonBar/RibbonBar.ts
+++ src/components/common/RibbonBar/RibbonBar.ts
@@ -1,10 +1,9 @@
-// RibbonBar.ts
 import Icon from '@/components/common/Icons/Icon.vue'
 import LinkCollection from '@/components/common/LinkCollection/LinkCollection.vue'
 import LoadingSpinner from '@/components/common/LoadingSpinner/LoadingSpinner.vue'
 import { listRepositoryTags } from '@/helpers/github-helper'
 import type { LinkType } from '@/types/common/Link'
-import { computed, defineComponent, onMounted, ref, type Ref } from 'vue'
+import { computed, defineComponent, onMounted, ref, watch, type Ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
@@ -15,14 +14,19 @@ export default defineComponent({
     LinkCollection
   },
   setup() {
+    const items = ref([
+      'The latest version of the website.',
+      'The previous version of the website.',
+      'The first version of the website.'
+    ])
     const { tm, rt } = useI18n()
     const tags = ref({ latest: undefined, previous: undefined }) as Ref<{
       latest: string | undefined
       previous: string | undefined
     }>
-
     const currentIndex = ref(0)
-    const totalItems = 3
+    const totalItems = ref(items.value.length)
+    const isTransitioning = ref(false)
 
     const fetchTags = async () => {
       const [latest, previous] = await listRepositoryTags({
@@ -44,25 +48,32 @@ export default defineComponent({
     })
 
     const scrollContent = (direction: 'left' | 'right') => {
+      isTransitioning.value = true
       if (direction === 'left') {
         currentIndex.value = Math.max(currentIndex.value - 1, 0)
       } else {
-        currentIndex.value = Math.min(currentIndex.value + 1, totalItems - 1)
+        currentIndex.value = Math.min(currentIndex.value + 1, totalItems.value - 1)
       }
     }
 
     const transformStyle = computed(() => {
-      const translateX = (currentIndex.value / totalItems) * -100
+      const transitionStyle = isTransitioning.value ? 'transform 1000ms ease 0s' : 'none 0s ease 0s'
       return {
-        transform: `translateX(${translateX}%)`,
-        width: '300%',
+        transform: `translateX(${-100 / totalItems.value}%)`,
+        width: `${100 * totalItems.value}%`,
         left: '-100%',
-        transition: 'transform 1000ms ease 0s'
+        transition: transitionStyle
       }
+    })
+
+    watch(currentIndex, () => {
+      setTimeout(() => {
+        isTransitioning.value = false
+      }, 1000)
     })
 
     onMounted(fetchTags)
 
-    return { links, tags, scrollContent, transformStyle }
+    return { items, links, tags, scrollContent, transformStyle }
   }
 })
