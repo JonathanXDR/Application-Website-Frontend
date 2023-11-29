4dc06b4df76896e18bb5cb56d43a5f3889201ab9
Jonathan Russ <jonathan.russ@bluewin.ch>
2023-11-29 07:26:12 +0100

    Update LanguageBarV2

--- src/components/common/LanguageBarV2/LanguageBarV2.ts
+++ src/components/common/LanguageBarV2/LanguageBarV2.ts
@@ -1,8 +1,12 @@
+import LinkCollection from '@/components/common/LinkCollection/LinkCollection.vue'
 import type { LanguageBarType } from '@/types/common/LanguageBar'
 import { defineComponent, type PropType } from 'vue'
 
 export default defineComponent({
   name: 'LanguageBarV2',
+  components: {
+    LinkCollection
+  },
   props: {
     language: {
       type: Object as PropType<LanguageBarType>,
--- src/components/common/LanguageBarV2/LanguageBarV2.vue
+++ src/components/common/LanguageBarV2/LanguageBarV2.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="column large-7 small-12 rs-od-iteminfo">
+  <div class="rs-od-iteminfo">
     <h2 class="rs-od-itemtitle">
-      <span class="rs-od-itemshortname">
-        {{ language.eyebrow }}
-      </span>
+      <span class="rs-od-itemshortname">{{ language.eyebrow }} </span>
       <span class="rs-od-itemstatus">{{ language.title }}</span>
     </h2>
-    <div class="rs-od-delivery-actions"></div>
+    <div class="rs-od-delivery-actions">
+      <LinkCollection :links="language.links" />
+    </div>
     <div
       id="rs-tracker-orderDetail.orderItems.orderItem-0000101.orderItemStatusTracker"
       class="rs-tracker as-l-container-full-small as-l-container-mobileoverflow"
@@ -15,7 +15,7 @@
         <div class="rs-tracker-bar">
           <div class="rs-tracker-fill" :style="`width: ${language.progress}%`"></div>
         </div>
-        <ol class="rs-tracker-status" role="list">
+        <ol class="rs-tracker-status">
           <li
             v-for="(item, index) in status"
             :key="index"
