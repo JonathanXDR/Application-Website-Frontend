13e98b9a35b1e44af654c04114bfe5059007b07c
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-01-07 12:31:07 +0100

    Update ColorSchemeToggle

--- components/common/ColorSchemeToggle.vue
+++ components/common/ColorSchemeToggle.vue
@@ -22,7 +22,6 @@
         <div class="text">Auto</div>
       </label>
     </div>
-    <script async="" src="/assets/scripts/color-scheme-toggle.js"></script>
   </div>
 </template>
 
@@ -31,34 +30,28 @@
 </script>
 
 <style scoped>
-html body .color-scheme-toggle {
+.color-scheme-toggle {
   border: 1px solid var(--toggle-color-text);
 }
 
-html body .color-scheme-toggle .text {
+.color-scheme-toggle .text {
   color: var(--toggle-color-text);
 }
 
-html body .color-scheme-toggle input:checked+.text {
+.color-scheme-toggle input:checked+.text {
   color: var(--color-button-text);
   background: var(--color-button-background);
   border-color: var(--color-button-background);
 }
 
-html body.dmf input {
+.dmf input {
   color: -internal-light-dark(white, black);
 }
 
-/*! CSS Used from: https://developer.apple.com/assets/styles/globalnav.css?23182355271 */
-input {
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -moz-font-feature-settings: "liga", "kern";
-}
+
 
 /*! CSS Used from: https://developer.apple.com/assets/styles/footer.dist.css?23182355271 */
-.footer-mini-news .content {
+.content {
   --toggle-border-radius-outer: 12px;
   --toggle-border-radius-inner: 10px;
   --toggle-color-fill: var(--color-button-background-active);
@@ -66,7 +59,7 @@ input {
   flex-shrink: 0;
 }
 
-.footer-mini-news .content .color-scheme-toggle {
+.content .color-scheme-toggle {
   font-size: 12px;
   line-height: 1.33337;
   font-weight: 400;
@@ -79,16 +72,16 @@ input {
 }
 
 @media screen {
-  [data-color-scheme="dark"] .footer-mini-news .content .color-scheme-toggle {
+  [data-color-scheme="dark"] .content .color-scheme-toggle {
     --toggle-color-text: var(--color-figure-blue);
   }
 }
 
-.footer-mini-news .content [role="radiogroup"]:focus {
+.content [role="radiogroup"]:focus {
   outline: none;
 }
 
-.footer-mini-news .content input[type="radio"] {
+.content input[type="radio"] {
   position: absolute;
   clip: rect(1px, 1px, 1px, 1px);
   clip-path: inset(0px 0px 99.9% 99.9%);
@@ -100,7 +93,7 @@ input {
   appearance: none;
 }
 
-.footer-mini-news .content .text {
+.content .text {
   box-sizing: border-box;
   display: inline-block;
   padding: 1px 6px;
@@ -111,11 +104,11 @@ input {
   color: var(--toggle-color-text);
 }
 
-.footer-mini-news .content .text:hover {
+.content .text:hover {
   cursor: pointer;
 }
 
-.footer-mini-news .content input[type="radio"]:checked+.text {
+.content input[type="radio"]:checked+.text {
   --toggle-color-text: var(--color-button-text);
   background: var(--toggle-color-fill);
   border-color: var(--toggle-color-fill);
