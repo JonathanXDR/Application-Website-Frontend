0f5f12a46bb99fd2287867581ba7dd1fdf0c5666
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-01-28 13:51:13 +0100

    Update i18n.config.ts

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,7 +1,7 @@
-import de from "~/lang/de-DE.json";
-import en from "~/lang/en-US.json";
-import fr from "~/lang/fr-FR.json";
-import it from "~/lang/it-IT.json";
+import de_DE from "~/lang/de-DE.json";
+import en_US from "~/lang/en-US.json";
+import fr_FR from "~/lang/fr-FR.json";
+import it_IT from "~/lang/it-IT.json";
 
 /** @type {import('i18n').Config} */
 export default {
@@ -9,21 +9,27 @@ export default {
   locales: [
     {
       code: "de",
+      iso: "de-DE",
       name: "Deutsch",
     },
     {
       code: "en",
+      iso: "en-US",
       name: "English",
     },
     {
       code: "fr",
+      iso: "fr-FR",
       name: "Français",
     },
     {
       code: "it",
+      iso: "it-IT",
       name: "Italiano",
     },
   ],
+  lazy: true,
+  langDir: "lang",
   defaultLocale: "de",
   fallbackLocale: "en",
   detectBrowserLanguage: {
@@ -32,9 +38,9 @@ export default {
     redirectOn: "root",
   },
   messages: {
-    en,
-    de,
-    fr,
-    it,
+    de: de_DE,
+    en: en_US,
+    fr: fr_FR,
+    it: it_IT,
   },
 };
