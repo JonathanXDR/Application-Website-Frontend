123922a882f47f2107ce724c57f128c556b54eb3
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-01-28 15:24:10 +0100

    Add additional dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n", "@nuxtjs/color-mode", "@nuxt/image"],
   css: ["@fortawesome/fontawesome-svg-core/styles.css"],
   devtools: {
     enabled: true,
