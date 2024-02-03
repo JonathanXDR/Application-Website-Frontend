8b81aecc37013195a1c579994afd2cd044d5696a
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-02-03 18:53:54 +0100

    Add interfaces to MusicKit helper

--- helpers/musickit-helper.ts
+++ helpers/musickit-helper.ts
@@ -1,8 +1,155 @@
-interface SearchResults {
-  albums: any[];
-  artists: any[];
-  songs: any[];
+interface MusicKitInstance {
+  api: MusicKitAPI;
+  player: MusicKitPlayer;
+  isAuthorized: boolean;
+  musicUserToken: string;
+  storefrontId: string;
+  developerToken: string;
+  authorize: () => Promise<string>;
+  unauthorize: () => Promise<void>;
 }
+
+interface MusicKitConfiguration {
+  developerToken: string;
+  app: AppConfiguration;
+}
+
+interface AppConfiguration {
+  name: string;
+  build: string;
+  version: string;
+  icon: string;
+}
+
+interface MusicKitPlayer {
+  play: () => Promise<void>;
+  pause: () => void;
+  stop: () => void;
+  skipToNextItem: () => void;
+  skipToPreviousItem: () => void;
+  setQueue: (options: SetQueueOptions) => Promise<void>;
+  nowPlayingItem: MediaItem;
+  playbackState: PlaybackState;
+  repeatMode: PlayerRepeatMode;
+  shuffleMode: PlayerShuffleMode;
+}
+
+interface SetQueueOptions {
+  album?: string;
+  playlist?: string;
+  song?: string;
+  songs?: string[];
+  url?: string;
+}
+
+interface Queue {
+  items: MediaItem[];
+  position: number;
+  nextPlayableItem: MediaItem;
+  previousPlayableItem: MediaItem;
+}
+
+interface MediaItem {
+  id: string;
+  type: "songs" | "albums" | "playlists";
+  attributes: MediaItemAttributes;
+}
+
+interface MediaItemAttributes {
+  albumName?: string;
+  artistName: string;
+  artwork: Artwork;
+  playParams: PlayParams;
+  genreNames: string[];
+  durationInMillis: number;
+  releaseDate: string;
+  name: string;
+  trackNumber?: number;
+}
+
+interface Artwork {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface PlayParams {
+  id: string;
+  kind: string;
+}
+
+interface MusicKitAPI {
+  album: (id: string) => Promise<Album>;
+  playlist: (id: string) => Promise<Playlist>;
+  song: (id: string) => Promise<Song>;
+  search: (term: string, options: SearchOptions) => Promise<SearchResults>;
+}
+
+interface Album {
+  attributes: AlbumAttributes;
+  id: string;
+  type: "albums";
+}
+
+interface Playlist {
+  attributes: PlaylistAttributes;
+  id: string;
+  type: "playlists";
+}
+
+interface Song {
+  attributes: SongAttributes;
+  id: string;
+  type: "songs";
+}
+
+interface Artist {
+  attributes: ArtistAttributes;
+  id: string;
+  type: "artists";
+}
+
+interface SearchOptions {
+  term: string;
+  limit?: number;
+  types: string[];
+}
+
+interface SearchResults {
+  albums: Album[];
+  playlists: Playlist[];
+  songs: Song[];
+  artists: Artist[];
+}
+
+interface AlbumAttributes extends MediaItemAttributes {
+  genreNames: string[];
+  isComplete: boolean;
+  isSingle: boolean;
+  trackCount: number;
+}
+
+interface PlaylistAttributes extends MediaItemAttributes {
+  curatorName: string;
+  description: string;
+  playParams: PlayParams;
+  playlistType: string;
+  trackCount: number;
+}
+
+interface SongAttributes extends MediaItemAttributes {
+  albumName: string;
+  discNumber: number;
+}
+
+interface ArtistAttributes {
+  genreNames: string[];
+  name: string;
+}
+
+type PlaybackState = "none" | "loading" | "playing" | "paused" | "stopped";
+type PlayerRepeatMode = "none" | "one" | "all";
+type PlayerShuffleMode = "off" | "songs";
 
 declare const MusicKit: any;
 
