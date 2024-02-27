0735b2203f2e615a1c7dffdf5daf6fd712fe8938
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-02-27 19:01:46 +0100

    Update BadgeType

--- types/common/Badge.ts
+++ types/common/Badge.ts
@@ -1,9 +1,10 @@
 import type { ColorType } from './Color'
 import type { LinkType } from './Link'
+import type { SizeType } from './Size'
 
 export interface BadgeType extends LinkType {
   variant?: keyof HTMLElementTagNameMap
-  size?: 'small' | 'medium' | 'large'
+  size?: Omit<SizeType, 'xsmall' & 'xlarge'>
   colors?: ColorType
   border?: boolean
   hover?: boolean
