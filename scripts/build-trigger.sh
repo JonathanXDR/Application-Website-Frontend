1d0f71c764a71b7081aa6cd3118979d2fcaedc6f
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-02-28 13:40:07 +0100

    Update build-trigger script

--- scripts/build-trigger.sh
+++ scripts/build-trigger.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 GITHUB_TOKEN=$VITE_GITHUB_TOKEN
+GITHUB_REPO_BRANCH=${GITHUB_REPO_BRANCH:-develop}
 BUILD_INTERVAL_MINUTES=${BUILD_INTERVAL_MINUTES:-30}
 MANUAL_TRIGGER_KEYWORD="force-build"
 
-latest_commit_timestamp=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
-    "https://api.github.com/repos/$GITHUB_REPO/commits?per_page=1" |
-    jq -r '.[0].commit.committer.date')
+latest_commit_info=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
+    "https://api.github.com/repos/$VITE_GITHUB_REPO_OWNER/$VITE_GITHUB_REPO_NAME/commits/$GITHUB_REPO_BRANCH?per_page=1")
+
+# curl -L \
+#     -H "Accept: application/vnd.github+json" \
+#     -H "Authorization: Bearer $GITHUB_TOKEN" \
+#     -H "X-GitHub-Api-Version: 2022-11-28" \
+#     https://api.github.com/repos/$VITE_GITHUB_REPO_OWNER/$VITE_GITHUB_REPO_NAME/commits/$GITHUB_REPO_BRANCH?per_page=1
+
+latest_commit_timestamp=$(echo "$latest_commit_info" | grep -oP '"date": "\K[^"]+' | head -1)
 
 latest_commit_unix=$(date -d "$latest_commit_timestamp" +%s)
 current_unix=$(date +%s)
@@ -14,9 +22,7 @@ minutes_diff=$((($current_unix - $latest_commit_unix) / 60))
 
 echo "Time since last commit: $minutes_diff minutes"
 
-commit_message=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
-    "https://api.github.com/repos/$VITE_GITHUB_REPO_OWNER/$VITE_GITHUB_REPO_NAME/commits?per_page=1" |
-    jq -r '.[0].commit.message')
+commit_message=$(echo "$latest_commit_info" | grep -oP '"message": "\K[^"]+' | head -1)
 
 if [[ "$commit_message" == *"$MANUAL_TRIGGER_KEYWORD"* ]]; then
     echo "✅ - Build triggered manually"
