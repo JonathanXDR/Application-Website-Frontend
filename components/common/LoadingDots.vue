9f0794b8e6b1838f3095e6b14bb7eded4c0d96bf
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-03-07 20:47:25 +0100

    Add LoadingDots & LoadingSkeleton

---
+++ components/common/LoadingDots.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="Loader_Loader__V2eds text-center">
+    <div class="Loader_PulseLoader__qJgQO">
+      <span style="display: inherit"
+        ><span
+          style="
+            background-color: rgb(0, 0, 0);
+            width: 8px;
+            height: 8px;
+            margin: 2px;
+            border-radius: 100%;
+            display: inline-block;
+            animation: 0.75s cubic-bezier(0.2, 0.68, 0.18, 1.08) 0.12s infinite
+              normal both running react-spinners-PulseLoader-pulse;
+          "
+        ></span
+        ><span
+          style="
+            background-color: rgb(0, 0, 0);
+            width: 8px;
+            height: 8px;
+            margin: 2px;
+            border-radius: 100%;
+            display: inline-block;
+            animation: 0.75s cubic-bezier(0.2, 0.68, 0.18, 1.08) 0.24s infinite
+              normal both running react-spinners-PulseLoader-pulse;
+          "
+        ></span
+        ><span
+          style="
+            background-color: rgb(0, 0, 0);
+            width: 8px;
+            height: 8px;
+            margin: 2px;
+            border-radius: 100%;
+            display: inline-block;
+            animation: 0.75s cubic-bezier(0.2, 0.68, 0.18, 1.08) 0.36s infinite
+              normal both running react-spinners-PulseLoader-pulse;
+          "
+        ></span
+      ></span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/*! CSS Used from:
+https://developer.apple.com/account/_next/static/css/f148b6570a351e02.css */ /*!
+@import https://developer.apple.com/assets/styles/global.dist.css */
+:focus {
+  outline: 4px solid rgba(0, 125, 250, 0.6);
+  outline-offset: 1px;
+}
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+.text-center {
+  text-align: center;
+}
+body main ::-webkit-scrollbar {
+  width: 16px;
+}
+body main ::-webkit-scrollbar-thumb {
+  border: 4px solid transparent;
+  border-radius: 9px;
+  background-clip: content-box;
+  border-color: var(--system-primary);
+  background: var(--system-secondary);
+}
+body main ::-webkit-scrollbar-thumb:hover {
+  background: var(--system-tertiary);
+}
+body main ::-webkit-scrollbar-track {
+  background: var(--system-primary);
+} /*! end
+@import */ /*! CSS Used from:
+https://developer.apple.com/account/_next/static/css/45bb825bfb9cd467.css */
+.dmf[data-color-scheme='dark'] .Loader_Loader__V2eds {
+  filter: invert(100%);
+}
+.Loader_PulseLoader__qJgQO {
+  opacity: 0.35;
+} /*! CSS Used from:
+https://developer.apple.com/account/_next/static/css/381b1f1850fab148.css */
+.dmf[data-color-scheme='dark'] .Loader_Loader__V2eds {
+  filter: invert(100%);
+}
+.Loader_PulseLoader__qJgQO {
+  opacity: 0.35;
+}
+</style>
---
+++ components/common/LoadingSkeleton.vue
@@ -0,0 +1,82 @@
+<template>
+  <span
+    class="react-loading-skeleton"
+    style="width: 175px; --base-color: #1d1d1f; --highlight-color: #262628"
+  ></span>
+</template>
+
+<style scoped>
+/*! CSS Used from:
+https://developer.apple.com/account/_next/static/css/f148b6570a351e02.css */ /*!
+@import https://developer.apple.com/assets/styles/global.dist.css */
+:focus {
+  outline: 4px solid rgba(0, 125, 250, 0.6);
+  outline-offset: 1px;
+}
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+body main ::-webkit-scrollbar {
+  width: 16px;
+}
+body main ::-webkit-scrollbar-thumb {
+  border: 4px solid transparent;
+  border-radius: 9px;
+  background-clip: content-box;
+  border-color: var(--system-primary);
+  background: var(--system-secondary);
+}
+body main ::-webkit-scrollbar-thumb:hover {
+  background: var(--system-tertiary);
+}
+body main ::-webkit-scrollbar-track {
+  background: var(--system-primary);
+} /*! end
+@import */ /*! CSS Used from:
+https://developer.apple.com/account/_next/static/css/381b1f1850fab148.css */
+.react-loading-skeleton {
+  --base-color: #ebebeb;
+  --highlight-color: #f5f5f5;
+  --animation-duration: 1.5s;
+  --animation-direction: normal;
+  --pseudo-element-display: block;
+  background-color: var(--base-color);
+  width: 100%;
+  border-radius: 0.25rem;
+  display: inline-flex;
+  line-height: 1;
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+}
+.react-loading-skeleton:after {
+  content: '';
+  display: var(--pseudo-element-display);
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-image: linear-gradient(
+    90deg,
+    var(--base-color),
+    var(--highlight-color),
+    var(--base-color)
+  );
+  transform: translateX(-100%);
+  animation-name: react-loading-skeleton;
+  animation-direction: var(--animation-direction);
+  animation-duration: var(--animation-duration);
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: infinite;
+}
+/*! CSS Used keyframes */
+@keyframes react-loading-skeleton {
+  to {
+    transform: translateX(100%);
+  }
+}
+</style>
