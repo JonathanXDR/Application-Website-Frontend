fc55f3577ca5152fe78d6bc9eafb8c11eb484acc
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-04-23 23:59:05 +0200

    Add FormAlert component

---
+++ components/common/FormAlert.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="form-alert is-error" role="alert">
+    <div class="rt-messages-text">
+      Die Autorisierung deiner Zahlung mit der •••9386 ist fehlgeschlagen. Bitte
+      prüfe deine Angaben und versuche es noch einmal. Oder wähle eine andere
+      Zahlungsmethode.
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.form-alert {
+  background-clip: padding-box;
+  background-color: #00000005;
+  border: 1px solid #d2d2d7;
+  border-radius: 12px;
+  color: #1d1d1f;
+  font-family: SF Pro Text, SF Pro Icons, Helvetica Neue, Helvetica, Arial,
+    sans-serif;
+  font-size: 17px;
+  font-weight: 400;
+  height: auto;
+  letter-spacing: -0.022em;
+  line-height: 1.2353641176;
+  margin-bottom: 17px;
+  margin-top: 17px;
+  padding: 0.7058823529rem 0.9411764706rem;
+  width: auto;
+}
+
+.form-alert.is-error {
+  background-color: #fff2f4;
+  border-color: #e3000066;
+}
+</style>
