9d594590c7fd0f663456a30b547cf94d241ef3be
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-05-02 21:37:29 +0200

    Update TimeLine

--- components/common/TimeLine.vue
+++ components/common/TimeLine.vue
@@ -35,39 +35,43 @@ const svg = ref<SVGElement | undefined>(undefined)
 const path = ref<SVGPathElement | undefined>(undefined)
 
 const initPath = () => {
-  const ulHeightRounded = Math.round(props.height || 0)
+  const instance = getCurrentInstance()
+  const ul = instance?.parent?.refs.ul as HTMLElement
+
+  const ulHeightValue = ul.getBoundingClientRect().height
+  const ulHeightRounded = Math.round(ulHeightValue)
 
   pathD.value = `M 4 0 L 4 ${ulHeightRounded}`
-  ulHeight.value = props.height
+  ulHeight.value = ulHeightValue
   viewBox.value = `0 0 8 ${ulHeightRounded}`
   xmlns.value = `http://www.w3.org/${ulHeightRounded}/svg`
 
-  strokeDasharray.value = props.height
-  strokeDashoffset.value = props.height
+  strokeDasharray.value = ulHeightValue
+  strokeDashoffset.value = ulHeightValue
 }
 
 const animateLine = () => {
+  const ulHeightValue = ulHeight.value || 0
   const center = window.innerHeight / 2
   const boundaries = path.value?.getBoundingClientRect()
 
-  const percentage
-    = (center - (boundaries?.top || 0)) / (boundaries?.height || 1)
-  const drawLength = percentage > 0 ? (props.height ?? 0) * percentage : 0
+  const percentage =
+    (center - (boundaries?.top || 0)) / (boundaries?.height || 1)
+  const drawLength = percentage > 0 ? ulHeightValue * percentage : 0
 
-  strokeDashoffset.value
-    = drawLength < (props.height ?? 0) ? (props.height ?? 0) - drawLength : 0
+  strokeDashoffset.value =
+    drawLength < ulHeightValue ? ulHeightValue - drawLength : 0
 }
 
 onMounted(() => {
   initPath()
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
       if (entry.isIntersecting) {
         window.addEventListener('scroll', animateLine)
         window.addEventListener('resize', initPath)
-      }
-      else {
+      } else {
         window.removeEventListener('scroll', animateLine)
         window.removeEventListener('resize', initPath)
       }
