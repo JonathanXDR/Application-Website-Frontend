d7cbe52f2891550a315ebfcb0637731fdf1e069d
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-06-02 22:38:39 +0200

    Update TechnologiesSection

--- components/containers/TechnologiesSection.vue
+++ components/containers/TechnologiesSection.vue
@@ -5,6 +5,28 @@
       class="typography-magical-headline pb-12"
     />
     <!-- <NavBarExtension>
+      <div class="flex flex-col items-center gap-2">
+        <SegmentNav
+          :items="segmentNavItems"
+          :label="windowWidth < 900 ? 'icon' : 'combination'"
+          :padding="windowWidth < 900 ? undefined : '0 21px'"
+          size="small"
+          :separator="windowWidth < 900 ? false : true"
+          gray-labels
+          :focus="false"
+          :outer-padding="3"
+          :selected-item="segmentNavItems[currentIndex]?.id"
+          :on-select="
+            id =>
+              updateCurrentIndex(
+                segmentNavItems.findIndex(item => item.id === id)
+              )
+          "
+        />
+      </div>
+    </NavBarExtension> -->
+
+    <!-- <NavBarExtension>
       <FilterInput />
     </NavBarExtension>
 
@@ -27,15 +49,26 @@
 
 <script setup lang="ts">
 import type { CardItemType } from '~/types/common/CardItem'
+import type { ItemType } from '~/types/common/Option'
 
 defineProps<{
   title: string
 }>()
 
 const { tm } = useI18n()
+
 const cards = computed<CardItemType[]>(() =>
   tm('components.containers.technologies')
 )
+const segmentNavItems = computed<ItemType[]>(() =>
+  tm('components.common.SegmentNav.technologies')
+)
+const { width: windowWidth } = useWindowSize({ initialWidth: 0 })
+const currentIndex = ref(0)
+
+const updateCurrentIndex = (index: number) => {
+  currentIndex.value = index
+}
 </script>
 
 <style scoped>
