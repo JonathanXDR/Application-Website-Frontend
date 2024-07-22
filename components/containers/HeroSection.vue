92c4db5870090f79befce70bd29cc34174ae183d
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-07-22 18:19:32 +0200

    Add HeroSection

---
+++ components/containers/HeroSection.vue
@@ -0,0 +1,159 @@
+<template>
+  <section class="hero_hero__E84_B">
+    <div class="hero_code__h0dLq" aria-hidden="true">
+      ======================================= :: 🔥🌸 Microkernel iBoot for d84,
+      Apple Inc. :: Local boot, Board 0x6 (d84ap)/Rev 0xd :: BUILD_TAG:
+      iBoot-10151.60.47.0.1 :: BUILD_STYLE: DEVELOPMENT :: USB_SERIAL_NUMBER:
+      SDOM:01 CPID:8130 CPRV:11 CPFM:01 SCEP:01 BDID:06 ECID:XXXXXXXXXXXXXXXX
+      IBFL:2D SIKA:00 SRNM:[XXXXXXXXXX] ::
+      =======================================
+    </div>
+    <div class="section-content hero_hero-content__GI7v6" style="opacity: 1">
+      <h1 class="hero_headline__WtfcM" style="opacity: 1; transform: none">
+        <em class="">Apple Security Research</em>
+      </h1>
+      <div class="hero_description__9aMj1" style="opacity: 1; transform: none">
+        <p>
+          Our groundbreaking security technologies protect the users of over 2.2
+          billion active devices around the world. Hear about the latest
+          advances in Apple security from our engineering teams, send us your
+          own research, and work directly with us to be recognized and rewarded
+          for helping keep our users safe.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.section-content {
+  width: calc(100% - 55px);
+  margin-inline: auto;
+}
+@media only screen and (min-width: 735px) {
+  .section-content {
+    width: 692px;
+  }
+}
+@media only screen and (min-width: 1069px) {
+  .section-content {
+    width: 990px;
+  }
+}
+/*! CSS Used from: https://security.apple.com/_next/static/css/87e47c498c059d9c.css */
+.hero_hero__E84_B {
+  padding-bottom: 60px;
+  overflow: hidden;
+}
+@media only screen and (min-width: 735px) {
+  .hero_hero__E84_B {
+    padding-bottom: 70px;
+  }
+}
+@media only screen and (min-width: 1069px) {
+  .hero_hero__E84_B {
+    padding-bottom: 75px;
+  }
+}
+.hero_hero__E84_B .hero_hero-content__GI7v6 {
+  color: #fff;
+}
+.hero_hero__E84_B .hero_hero-content__GI7v6 .hero_headline__WtfcM {
+  font-size: 80px;
+  line-height: 1.05;
+  font-weight: 600;
+  letter-spacing: -0.015em;
+  font-family: SF Pro Display, SF Pro Icons, Helvetica Neue, Helvetica, Arial,
+    sans-serif;
+  margin: 0;
+  color: inherit;
+  text-align: left;
+}
+@media only screen and (max-width: 1068px) {
+  .hero_hero__E84_B .hero_hero-content__GI7v6 .hero_headline__WtfcM {
+    font-size: 64px;
+    line-height: 1.0625;
+    font-weight: 600;
+    letter-spacing: -0.009em;
+    font-family: SF Pro Display, SF Pro Icons, Helvetica Neue, Helvetica, Arial,
+      sans-serif;
+  }
+}
+@media only screen and (max-width: 734px) {
+  .hero_hero__E84_B .hero_hero-content__GI7v6 .hero_headline__WtfcM {
+    font-size: 44px;
+    line-height: 1.1818181818;
+    font-weight: 600;
+    letter-spacing: -0.00066em;
+    font-family: SF Pro Display, SF Pro Icons, Helvetica Neue, Helvetica, Arial,
+      sans-serif;
+  }
+}
+@media only screen and (min-width: 735px) {
+  .hero_hero__E84_B .hero_hero-content__GI7v6 .hero_headline__WtfcM {
+    text-align: center;
+  }
+}
+.hero_hero__E84_B .hero_hero-content__GI7v6 .hero_headline__WtfcM em {
+  font-style: normal;
+  background: linear-gradient(90deg, #d2feb4, #12cabf);
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.hero_hero__E84_B .hero_hero-content__GI7v6 .hero_description__9aMj1 {
+  margin: 20px auto 0;
+  font-size: 24px;
+  line-height: 1.3334133333;
+  font-weight: 400;
+  letter-spacing: 0.009em;
+  font-family: SF Pro Display, SF Pro Icons, Helvetica Neue, Helvetica, Arial,
+    sans-serif;
+  text-align: left;
+}
+@media only screen and (max-width: 1068px) {
+  .hero_hero__E84_B .hero_hero-content__GI7v6 .hero_description__9aMj1 {
+    font-size: 19px;
+    line-height: 1.4211026316;
+    font-weight: 400;
+    letter-spacing: 0.012em;
+    font-family: SF Pro Display, SF Pro Icons, Helvetica Neue, Helvetica, Arial,
+      sans-serif;
+  }
+}
+@media only screen and (min-width: 735px) {
+  .hero_hero__E84_B .hero_hero-content__GI7v6 .hero_description__9aMj1 {
+    text-align: center;
+    max-width: 580px;
+  }
+}
+@media only screen and (min-width: 1069px) {
+  .hero_hero__E84_B .hero_hero-content__GI7v6 .hero_description__9aMj1 {
+    margin-top: 30px;
+    max-width: 920px;
+  }
+}
+.hero_code__h0dLq {
+  width: 150%;
+  height: 3em;
+  font-size: 1.8823529412rem;
+  line-height: 1.5;
+  font-weight: 300;
+  letter-spacing: -0.02em;
+  font-family: SF Mono, SF Pro Icons, Menlo, monospace;
+  opacity: 0.2;
+  color: #e8e8ed;
+  -webkit-mask-image: linear-gradient(0deg, transparent, #eee 120%);
+  mask-image: linear-gradient(0deg, transparent, #eee 120%);
+}
+@media only screen and (min-width: 735px) {
+  .hero_code__h0dLq {
+    margin-top: -16px;
+    height: 4.7619047619em;
+    font-size: 2.4705882353rem;
+    line-height: 1.1904761905;
+  }
+}
+</style>
