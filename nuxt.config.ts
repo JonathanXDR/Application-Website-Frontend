1d1bef5dd25c39eb09d57e83870a633026224490
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-07-29 14:07:32 +0200

    Cleanup nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,7 +50,7 @@ export default defineNuxtConfig({
       type: "Person",
       name: process.env.SITE_NAME || "Jonathan Russ",
       url: process.env.SITE_URL,
-      logo: "/img/favicon.png",
+      logo: process.env.SITE_LOGO,
     },
   },
 
@@ -63,18 +63,24 @@ export default defineNuxtConfig({
           description: process.env.SITE_DESCRIPTION,
         },
       },
+      link: [{ rel: "apple-touch-icon", href: process.env.SITE_LOGO }],
       meta: [
         { name: "title", content: process.env.SITE_NAME },
         {
           name: "description",
           content: process.env.SITE_DESCRIPTION,
         },
+        { property: "og:image", content: process.env.SITE_LOGO },
         { property: "og:title", content: process.env.SITE_NAME },
         {
           property: "og:description",
           content: process.env.SITE_DESCRIPTION,
         },
         { property: "og:url", content: `${process.env.SITE_URL}/en` },
+        {
+          property: "twitter:image",
+          content: process.env.SITE_LOGO,
+        },
         { property: "twitter:card", content: "summary_large_image" },
         { property: "twitter:title", content: process.env.SITE_NAME },
         {
@@ -115,14 +121,6 @@ export default defineNuxtConfig({
             type: "image/svg+xml",
             href: "data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22256%22%20height%3D%22256%22%20viewBox%3D%220%200%20100%20100%22%3E%0A%20%20%20%20%3Crect%20width%3D%22100%22%20height%3D%22100%22%20rx%3D%2220%22%20fill%3D%22%23f56300%22%3E%3C%2Frect%3E%0A%20%20%20%20%3Cpath%20fill%3D%22%23ffffff%22%0A%20%20%20%20%20%20%20%20d%3D%22M26.30%2069.58Q21.90%2069.58%2018.71%2068.09Q15.52%2066.61%2013.76%2063.80L13.76%2063.80L19.64%2058.20Q22.17%2062.27%2026.41%2062.27L26.41%2062.27Q31.58%2062.27%2032.79%2056.27L32.79%2056.27L36.47%2037.57L23.00%2037.57L24.43%2030.42L46.76%2030.42L41.64%2055.83Q40.21%2063.20%2036.47%2066.39Q32.73%2069.58%2026.30%2069.58L26.30%2069.58ZM86.25%2042.36Q86.25%2047.75%2083.33%2051.59Q80.41%2055.45%2075.25%2057.04L75.25%2057.04L81.95%2068.92L72.55%2068.92L66.50%2058.14L58.91%2058.14L56.77%2068.92L47.80%2068.92L55.50%2030.42L71.17%2030.42Q78.32%2030.42%2082.28%2033.55Q86.25%2036.69%2086.25%2042.36L86.25%2042.36ZM67.88%2051.05Q72.33%2051.05%2074.78%2049.01Q77.22%2046.98%2077.22%2043.18L77.22%2043.18Q77.22%2040.43%2075.35%2039.06Q73.48%2037.68%2070.02%2037.68L70.02%2037.68L62.98%2037.68L60.28%2051.05L67.88%2051.05Z%22%3E%0A%20%20%20%20%3C%2Fpath%3E%0A%3C%2Fsvg%3E",
           },
-          { rel: "apple-touch-icon", href: "/img/dev/favicon-dev-orange.png" },
-        ],
-        meta: [
-          { property: "og:image", content: "/img/dev/favicon-dev-orange.png" },
-          {
-            property: "twitter:image",
-            content: `${process.env.SITE_URL}/img/favicon.png`,
-          },
         ],
       },
     },
@@ -155,14 +153,10 @@ export default defineNuxtConfig({
     app: {
       head: {
         link: [
-          { rel: "icon", type: "image/svg+xml", href: "/img/favicon.svg" },
-          { rel: "apple-touch-icon", href: "/img/favicon.png" },
-        ],
-        meta: [
-          { property: "og:image", content: "/img/favicon.png" },
           {
-            property: "twitter:image",
-            content: `${process.env.SITE_URL}/img/favicon.png`,
+            rel: "icon",
+            type: "image/svg+xml",
+            href: process.env.SITE_LOGO,
           },
         ],
       },
@@ -187,6 +181,8 @@ export default defineNuxtConfig({
     public: {
       appName: process.env.APP_NAME,
       appBuild: process.env.APP_BUILD,
+      appVersion: process.env.APP_VERSION,
+      appIcon: process.env.APP_ICON,
       appEnvironment: process.env.NODE_ENV,
 
       githubRepoName: process.env.GITHUB_REPO_NAME,
@@ -253,7 +249,7 @@ export default defineNuxtConfig({
           "https://*.googletagmanager.com",
           "https://snippet.meticulous.ai",
           "https://browser.sentry-cdn.com",
-          "https://*.music.apple.com",
+          "https://*.apple.com",
         ],
         "img-src": [
           "'self'",
@@ -263,7 +259,7 @@ export default defineNuxtConfig({
           "https://*.googletagmanager.com",
           "https://*.g.doubleclick.net",
           "https://*.google.com",
-          "https://*.music.apple.com",
+          "https://*.apple.com",
         ],
         "connect-src": [
           "'self'",
@@ -276,7 +272,7 @@ export default defineNuxtConfig({
           "https://user-events-v3.s3-accelerate.amazonaws.com",
           "https://edge.hypertune.com",
           "https://gcp.fasthorse.workers.dev",
-          "https://*.music.apple.com",
+          "https://*.apple.com",
           "*.sentry.io",
         ],
       },
