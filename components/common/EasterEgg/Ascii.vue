a5378b0c5f5d95daaf3ebc09db039e14dfd706a2
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-08-09 19:19:31 +0200

    Add Ascii component

---
+++ components/common/EasterEgg/Ascii.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <pre>
+        <code>
+        {{ randomAscii() }}
+        </code>
+    </pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const asciiArts = ref([]);
+
+function randomAscii() {
+  const randomIndex = Math.floor(Math.random() * asciiArts.value.length);
+  return asciiArts.value[randomIndex];
+}
+</script>
