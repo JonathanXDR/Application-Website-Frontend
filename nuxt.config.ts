56d369ce6d9ad6a8fcbc3883fe1b5ff3d511e5aa
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-10-14 15:24:19 +0200

    Update nuxt.config.ts layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,27 +16,81 @@ export default defineNuxtConfig({
     'nuxt-viewport',
   ],
 
-  css: ['~/assets/css/main.css'],
+  $development: {
 
-  fonts: {
-    defaults: {
-      weights: [400, 500, 600, 700],
-      styles: ['normal'],
-      subsets: ['latin'],
-    },
-    families: [
-      {
-        name: 'system-ui',
-        provider: 'none',
-        fallbacks: [
-          '-apple-system',
-          'BlinkMacSystemFont',
-          '"Helvetica Neue"',
-          'Helvetica',
-          'Arial',
-          'sans-serif',
+    app: {
+      head: {
+        link: [
+          {
+            rel: 'icon',
+            type: 'image/svg+xml',
+            href: 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22256%22%20height%3D%22256%22%20viewBox%3D%220%200%20100%20100%22%3E%0A%20%20%20%20%3Crect%20width%3D%22100%22%20height%3D%22100%22%20rx%3D%2220%22%20fill%3D%22%23f56300%22%3E%3C%2Frect%3E%0A%20%20%20%20%3Cpath%20fill%3D%22%23ffffff%22%0A%20%20%20%20%20%20%20%20d%3D%22M26.30%2069.58Q21.90%2069.58%2018.71%2068.09Q15.52%2066.61%2013.76%2063.80L13.76%2063.80L19.64%2058.20Q22.17%2062.27%2026.41%2062.27L26.41%2062.27Q31.58%2062.27%2032.79%2056.27L32.79%2056.27L36.47%2037.57L23.00%2037.57L24.43%2030.42L46.76%2030.42L41.64%2055.83Q40.21%2063.20%2036.47%2066.39Q32.73%2069.58%2026.30%2069.58L26.30%2069.58ZM86.25%2042.36Q86.25%2047.75%2083.33%2051.59Q80.41%2055.45%2075.25%2057.04L75.25%2057.04L81.95%2068.92L72.55%2068.92L66.50%2058.14L58.91%2058.14L56.77%2068.92L47.80%2068.92L55.50%2030.42L71.17%2030.42Q78.32%2030.42%2082.28%2033.55Q86.25%2036.69%2086.25%2042.36L86.25%2042.36ZM67.88%2051.05Q72.33%2051.05%2074.78%2049.01Q77.22%2046.98%2077.22%2043.18L77.22%2043.18Q77.22%2040.43%2075.35%2039.06Q73.48%2037.68%2070.02%2037.68L70.02%2037.68L62.98%2037.68L60.28%2051.05L67.88%2051.05Z%22%3E%0A%20%20%20%20%3C%2Fpath%3E%0A%3C%2Fsvg%3E',
+          },
         ],
       },
+    },
+
+    scripts: {
+      globals: {
+        meticulousAi: {
+          src: 'https://snippet.meticulous.ai/v1/meticulous.js',
+          'data-project-id': '3xUUe4R1NNzA6BJE6HKzrGCjCRddpahZJeJh8N0w',
+          'data-is-production-environment': false,
+        },
+      },
+    },
+    security: {
+      headers: {
+        crossOriginEmbedderPolicy: 'unsafe-none',
+        contentSecurityPolicy: {
+          'upgrade-insecure-requests': false,
+        },
+      },
+    },
+  },
+
+  $production: {
+
+    app: {
+      head: {
+        link: [
+          { rel: 'icon', type: 'image/svg+xml', href: '/img/favicon.svg' },
+        ],
+      },
+    },
+
+    scripts: {
+      globals: {
+        meticulousAi: {
+          src: 'https://snippet.meticulous.ai/v1/meticulous.js',
+          'data-project-id': '3xUUe4R1NNzA6BJE6HKzrGCjCRddpahZJeJh8N0w',
+          'data-is-production-environment': true,
+        },
+      },
+    },
+
+    security: {
+      headers: {
+        crossOriginEmbedderPolicy: 'require-corp',
+        contentSecurityPolicy: {
+          'upgrade-insecure-requests': true,
+        },
+      },
+    },
+    seo: {
+      redirectToCanonicalSiteUrl: true,
+    },
+  },
+
+  ssr: true,
+
+  components: {
+    dirs: [
+      {
+        path: '~/components/containers',
+        global: true,
+      },
+      '~/components/common',
     ],
   },
 
@@ -44,38 +98,6 @@ export default defineNuxtConfig({
     enabled: true,
     timeline: {
       enabled: true,
-    },
-  },
-
-  typescript: {
-    typeCheck: true,
-    strict: true,
-  },
-
-  eslint: {
-    config: {
-      tooling: true,
-      stylistic: true,
-      typescript: {
-        strict: true,
-      },
-    },
-  },
-
-  ssr: true,
-
-  compatibilityDate: '2024-07-03',
-
-  future: {
-    compatibilityVersion: 4,
-  },
-
-  schemaOrg: {
-    identity: {
-      type: 'Person',
-      name: process.env.SITE_NAME || 'Jonathan Russ',
-      url: process.env.SITE_URL,
-      logo: process.env.SITE_LOGO,
     },
   },
 
@@ -121,88 +143,20 @@ export default defineNuxtConfig({
     },
   },
 
-  $development: {
-    security: {
-      headers: {
-        crossOriginEmbedderPolicy: 'unsafe-none',
-        contentSecurityPolicy: {
-          'upgrade-insecure-requests': false,
-        },
-      },
-    },
-
-    app: {
-      head: {
-        link: [
-          {
-            rel: 'icon',
-            type: 'image/svg+xml',
-            href: 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22256%22%20height%3D%22256%22%20viewBox%3D%220%200%20100%20100%22%3E%0A%20%20%20%20%3Crect%20width%3D%22100%22%20height%3D%22100%22%20rx%3D%2220%22%20fill%3D%22%23f56300%22%3E%3C%2Frect%3E%0A%20%20%20%20%3Cpath%20fill%3D%22%23ffffff%22%0A%20%20%20%20%20%20%20%20d%3D%22M26.30%2069.58Q21.90%2069.58%2018.71%2068.09Q15.52%2066.61%2013.76%2063.80L13.76%2063.80L19.64%2058.20Q22.17%2062.27%2026.41%2062.27L26.41%2062.27Q31.58%2062.27%2032.79%2056.27L32.79%2056.27L36.47%2037.57L23.00%2037.57L24.43%2030.42L46.76%2030.42L41.64%2055.83Q40.21%2063.20%2036.47%2066.39Q32.73%2069.58%2026.30%2069.58L26.30%2069.58ZM86.25%2042.36Q86.25%2047.75%2083.33%2051.59Q80.41%2055.45%2075.25%2057.04L75.25%2057.04L81.95%2068.92L72.55%2068.92L66.50%2058.14L58.91%2058.14L56.77%2068.92L47.80%2068.92L55.50%2030.42L71.17%2030.42Q78.32%2030.42%2082.28%2033.55Q86.25%2036.69%2086.25%2042.36L86.25%2042.36ZM67.88%2051.05Q72.33%2051.05%2074.78%2049.01Q77.22%2046.98%2077.22%2043.18L77.22%2043.18Q77.22%2040.43%2075.35%2039.06Q73.48%2037.68%2070.02%2037.68L70.02%2037.68L62.98%2037.68L60.28%2051.05L67.88%2051.05Z%22%3E%0A%20%20%20%20%3C%2Fpath%3E%0A%3C%2Fsvg%3E',
-          },
-        ],
-      },
-    },
-
-    scripts: {
-      globals: {
-        meticulousAi: {
-          src: 'https://snippet.meticulous.ai/v1/meticulous.js',
-          'data-project-id': '3xUUe4R1NNzA6BJE6HKzrGCjCRddpahZJeJh8N0w',
-          'data-is-production-environment': false,
-        },
-      },
-    },
-  },
-
-  $production: {
-    seo: {
-      redirectToCanonicalSiteUrl: true,
-    },
-
-    security: {
-      headers: {
-        crossOriginEmbedderPolicy: 'require-corp',
-        contentSecurityPolicy: {
-          'upgrade-insecure-requests': true,
-        },
-      },
-    },
-
-    app: {
-      head: {
-        link: [
-          { rel: 'icon', type: 'image/svg+xml', href: '/img/favicon.svg' },
-        ],
-      },
-    },
-
-    scripts: {
-      globals: {
-        meticulousAi: {
-          src: 'https://snippet.meticulous.ai/v1/meticulous.js',
-          'data-project-id': '3xUUe4R1NNzA6BJE6HKzrGCjCRddpahZJeJh8N0w',
-          'data-is-production-environment': true,
-        },
-      },
-    },
-  },
+  css: ['~/assets/css/main.css'],
 
   router: {
     options: routerOptions,
   },
 
-  viewport: {
-    breakpoints: {
-      mobile: 320,
-      mobileMedium: 475,
-      mobileWide: 640,
-      tablet: 768,
-      desktop: 1024,
-      desktopMedium: 1280,
-      desktopWide: 1536,
-    },
+  site: {
+    url: process.env.SITE_URL,
+    name: process.env.SITE_NAME,
+    description: process.env.SITE_DESCRIPTION,
+  },
 
-    fallbackBreakpoint: 'lg',
+  colorMode: {
+    classSuffix: '',
   },
 
   runtimeConfig: {
@@ -225,6 +179,102 @@ export default defineNuxtConfig({
     appleMusicUserToken: process.env.APPLE_MUSIC_USER_TOKEN,
   },
 
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  compatibilityDate: '2024-10-14',
+
+  typescript: {
+    typeCheck: true,
+    strict: true,
+  },
+
+  eslint: {
+    config: {
+      tooling: true,
+      stylistic: true,
+      typescript: {
+        strict: true,
+      },
+    },
+  },
+
+  fonts: {
+    defaults: {
+      weights: [400, 500, 600, 700],
+      styles: ['normal'],
+      subsets: ['latin'],
+    },
+    families: [
+      {
+        name: 'system-ui',
+        provider: 'none',
+        fallbacks: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Helvetica Neue"',
+          'Helvetica',
+          'Arial',
+          'sans-serif',
+        ],
+      },
+    ],
+  },
+
+  i18n: {
+    baseUrl: process.env.SITE_URL,
+    vueI18n: '~/i18n.config.ts',
+    // lazy: true,
+    // langDir: 'lang',
+    strategy: 'prefix_and_default',
+    defaultLocale: 'de',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieCrossOrigin: true,
+      cookieDomain: process.env.SITE_DOMAIN,
+      cookieKey: 'i18n_redirected',
+      cookieSecure: true,
+      fallbackLocale: 'en',
+      redirectOn: 'root',
+    },
+    locales: [
+      {
+        code: 'de',
+        language: 'de-DE',
+        name: 'Deutsch',
+        // file: 'de.json'
+      },
+      {
+        code: 'en',
+        language: 'en-US',
+        name: 'English',
+        // file: 'en.json'
+      },
+      {
+        code: 'fr',
+        language: 'fr-FR',
+        name: 'Français',
+        // file: 'fr.json'
+      },
+      {
+        code: 'it',
+        language: 'it-IT',
+        name: 'Italiano',
+        // file: 'it.json'
+      },
+    ],
+  },
+
+  schemaOrg: {
+    identity: {
+      type: 'Person',
+      name: process.env.SITE_NAME || 'Jonathan Russ',
+      url: process.env.SITE_URL,
+      logo: process.env.SITE_LOGO,
+    },
+  },
+
   scripts: {
     defaultScriptOptions: {
       bundle: true,
@@ -244,26 +294,6 @@ export default defineNuxtConfig({
         'data-web-components': true,
       },
     },
-  },
-
-  components: {
-    dirs: [
-      {
-        path: '~/components/containers',
-        global: true,
-      },
-      '~/components/common',
-    ],
-  },
-
-  colorMode: {
-    classSuffix: '',
-  },
-
-  site: {
-    url: process.env.SITE_URL,
-    name: process.env.SITE_NAME,
-    description: process.env.SITE_DESCRIPTION,
   },
 
   security: {
@@ -311,47 +341,17 @@ export default defineNuxtConfig({
     },
   },
 
-  i18n: {
-    baseUrl: process.env.SITE_URL,
-    vueI18n: '~/i18n.config.ts',
-    // lazy: true,
-    // langDir: 'lang',
-    strategy: 'prefix_and_default',
-    defaultLocale: 'de',
-    detectBrowserLanguage: {
-      useCookie: true,
-      cookieCrossOrigin: true,
-      cookieDomain: process.env.SITE_DOMAIN,
-      cookieKey: 'i18n_redirected',
-      cookieSecure: true,
-      fallbackLocale: 'en',
-      redirectOn: 'root',
+  viewport: {
+    breakpoints: {
+      mobile: 320,
+      mobileMedium: 475,
+      mobileWide: 640,
+      tablet: 768,
+      desktop: 1024,
+      desktopMedium: 1280,
+      desktopWide: 1536,
     },
-    locales: [
-      {
-        code: 'de',
-        language: 'de-DE',
-        name: 'Deutsch',
-        // file: 'de.json'
-      },
-      {
-        code: 'en',
-        language: 'en-US',
-        name: 'English',
-        // file: 'en.json'
-      },
-      {
-        code: 'fr',
-        language: 'fr-FR',
-        name: 'Français',
-        // file: 'fr.json'
-      },
-      {
-        code: 'it',
-        language: 'it-IT',
-        name: 'Italiano',
-        // file: 'it.json'
-      },
-    ],
+
+    fallbackBreakpoint: 'lg',
   },
 })
