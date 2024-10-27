aa722f4fb188acb24d2b75050bf6a3c9aa490de9
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-10-27 19:53:13 +0100

    Remove eslint-config-prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,10 @@
 import type { Linter } from "eslint";
-import configPrettier from "eslint-config-prettier";
 import pluginPrettier from "eslint-plugin-prettier/recommended";
 import neostandard from "neostandard";
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
 const config: Linter.Config[] = [
   ...neostandard(),
-  configPrettier,
   pluginPrettier,
   {
     rules: {
