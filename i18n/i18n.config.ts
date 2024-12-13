af785e2488bd306eb8914a93b8f6fa6808940f2d
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-12-13 22:36:07 +0100

    Migrate to i18n directory

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -2,20 +2,17 @@ import 'dayjs/locale/de'
 import 'dayjs/locale/en'
 import 'dayjs/locale/fr'
 import 'dayjs/locale/it'
-import de from './lang/de.json'
-import en from './lang/en.json'
-import fr from './lang/fr.json'
-import it from './lang/it.json'
+import de from './locales/de.json'
+import en from './locales/en.json'
+import fr from './locales/fr.json'
+import it from './locales/it.json'
 
 export default defineI18nConfig(() => {
-  const locale = 'de'
-  const fallbackLocale = 'en'
-
   return {
     legacy: false,
     globalInjection: true,
-    locale,
-    fallbackLocale,
+    locale: 'de',
+    fallbackLocale: 'en',
     messages: {
       de,
       en,
