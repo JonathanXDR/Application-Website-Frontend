d8f0f5ce577a74e6f758f0359bad658e5b0d12cd
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-12-13 22:52:15 +0100

    Revert "Migrate to i18n directory"
    This reverts commit af785e2488bd306eb8914a93b8f6fa6808940f2d.

--- i18n.config.ts
+++ i18n.config.ts
@@ -2,17 +2,20 @@ import 'dayjs/locale/de'
 import 'dayjs/locale/en'
 import 'dayjs/locale/fr'
 import 'dayjs/locale/it'
-import de from './locales/de.json'
-import en from './locales/en.json'
-import fr from './locales/fr.json'
-import it from './locales/it.json'
+import de from './lang/de.json'
+import en from './lang/en.json'
+import fr from './lang/fr.json'
+import it from './lang/it.json'
 
 export default defineI18nConfig(() => {
+  const locale = 'de'
+  const fallbackLocale = 'en'
+
   return {
     legacy: false,
     globalInjection: true,
-    locale: 'de',
-    fallbackLocale: 'en',
+    locale,
+    fallbackLocale,
     messages: {
       de,
       en,
