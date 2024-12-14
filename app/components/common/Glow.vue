c5464f2d723e1d21b91b81769bd2f5f91af79ec4
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-12-14 17:42:30 +0100

    Update Glow

--- app/components/common/Glow.vue
+++ app/components/common/Glow.vue
@@ -4,12 +4,6 @@
 </template>
 
 <style scoped>
-/*! CSS Used from: https://www.apple.com/v/macbook-pro/an/built/styles/main.built.css */
-:focus-visible {
-  outline: 2px solid var(--sk-focus-color, #0071e3);
-  outline-offset: var(--sk-focus-offset, 1px);
-}
-/*! CSS Used from: https://www.apple.com/v/macbook-pro/an/built/styles/overview.built.css */
 * {
   --mx-pro-blue: #2997ff;
   --mx-max-purple: #a972ff;
@@ -36,6 +30,22 @@
   border: 0;
   filter: blur(1px);
 }
+/* .apple-intelligence-glow {
+  --igradient: linear-gradient(
+    115deg,
+    #00d0ff,
+    #09f 20%,
+    #c95eff 42%,
+    #ff2569 63%,
+    #ff8300 83%,
+    #ffb000
+  );
+  position: absolute;
+  z-index: 0;
+  inset: 0;
+  border: 0;
+  filter: blur(1px);
+} */
 .apple-intelligence-glow::after {
   --bg-size: calc(100% + 14px);
   content: '';
@@ -50,14 +60,30 @@
   mask-clip: padding-box, border-box;
   mask-composite: intersect;
 }
+/* .apple-intelligence-glow::after {
+  --bg-size: calc(100% + 14px);
+  content: '';
+  pointer-events: none;
+  position: absolute;
+  inset: 0;
+  background: var(--igradient) 50% / var(--bg-size) var(--bg-size);
+  border-radius: 28px;
+  mask: linear-gradient(hsla(0, 0%, 100%, 0), hsla(0, 0%, 100%, 0)),
+    linear-gradient(hsl(0, 0%, 100%), hsl(0, 0%, 100%));
+  mask-clip: padding-box, border-box;
+  mask-composite: intersect;
+} */
 .apple-intelligence-glow.apple-intelligence-glow-2 {
   filter: blur(7px);
 }
+/* .apple-intelligence-glow.apple-intelligence-glow-2 {
+  filter: blur(9px);
+} */
 .apple-intelligence-glow.apple-intelligence-glow-2::after {
   border: 8px solid rgba(0, 0, 0, 0);
 }
 @media (max-width: 734px) {
-  .subsection-apple-intelligence .apple-intelligence-glow {
+  .apple-intelligence-glow {
     --igradient: linear-gradient(
       90deg,
       #0894ff 0%,
@@ -69,7 +95,7 @@
   }
 }
 @media (max-width: 734px) {
-  .subsection-apple-intelligence .apple-intelligence-glow::after {
+  .apple-intelligence-glow::after {
     border-radius: 0;
     inset-inline: -20px;
   }
