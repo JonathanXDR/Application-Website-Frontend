0c4faaef187f5a8e480af2c4bd96c991d64a87c7
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-12-26 19:01:39 +0100

    Cleanup HyperText

--- app/components/common/HyperText.vue
+++ app/components/common/HyperText.vue
@@ -4,13 +4,12 @@
     ref="elementRef"
     class="overflow-hidden py-2 text-4xl font-bold"
     :class="props.class"
-    v-bind="$attrs"
     @mouseenter="handleAnimationTrigger"
   >
     <span
       v-for="(letter, index) in displayText"
       :key="index"
-      :class="['font-mono', letter === ' ' ? 'w-3 inline-block' : '']"
+      :class="['font-mono', letter === ' ' ? 'inline-block w-3' : '']"
     >
       {{ letter.toUpperCase() }}
     </span>
@@ -39,11 +38,13 @@ const props = withDefaults(defineProps<HyperTextProps>(), {
 })
 
 const elementRef = ref<HTMLElement | null>(null)
-const displayText = ref<string[]>(props.text.split(''))
+const displayText = ref(props.text.split(''))
 const isAnimating = ref(false)
 const iterationCount = ref(0)
 
-function getRandomInt(max: number): number {
+let totalCharacters = props.text.length
+
+function getRandomInt(max: number) {
   return Math.floor(Math.random() * max)
 }
 
@@ -54,67 +55,64 @@ function handleAnimationTrigger() {
   }
 }
 
-onMounted(() => {
-  if (!props.startOnView) {
-    const startTimeout = setTimeout(() => {
-      isAnimating.value = true
-    }, props.delay)
+function scramble() {
+  if (iterationCount.value < totalCharacters) {
+    displayText.value = displayText.value.map((char, index) => {
+      if (char === ' ') return ' '
 
-    onBeforeUnmount(() => clearTimeout(startTimeout))
+      if (index <= iterationCount.value) {
+        return props.text[index]
+      }
+      return props.characterSet[getRandomInt(props.characterSet.length)]
+    }) as string[]
+    iterationCount.value += 0.1
   }
   else {
-    if (elementRef.value) {
-      const { stop } = useIntersectionObserver(
-        elementRef,
-        ([entry]) => {
-          if (entry?.isIntersecting) {
-            setTimeout(() => {
-              isAnimating.value = true
-            }, props.delay)
-            stop()
-          }
-        },
-        { threshold: 0.1, rootMargin: '-30% 0px -30% 0px' },
-      )
-    }
+    isAnimating.value = false
   }
+}
+
+const intervalMs = ref(0)
+const { pause, resume } = useIntervalFn(scramble, intervalMs, {
+  immediate: false,
 })
 
-let intervalId: number | null = null
+onMounted(() => {
+  if (!props.startOnView) {
+    useTimeoutFn(() => {
+      isAnimating.value = true
+    }, props.delay)
+  }
+  else if (elementRef.value) {
+    const { stop } = useIntersectionObserver(
+      elementRef,
+      ([entry]) => {
+        if (entry?.isIntersecting) {
+          useTimeoutFn(() => {
+            isAnimating.value = true
+          }, props.delay)
+
+          stop()
+        }
+      },
+      {
+        threshold: 0.1,
+        rootMargin: '-30% 0px -30% 0px',
+      },
+    )
+  }
+})
 
 watch(isAnimating, (newVal) => {
   if (newVal) {
-    const intervalDuration = props.duration / (props.text.length * 10)
-    const maxIterations = props.text.length
-
-    intervalId = window.setInterval(() => {
-      if (iterationCount.value < maxIterations) {
-        displayText.value = displayText.value.map((char, index) => {
-          if (char === ' ') return char
-          if (index <= iterationCount.value) {
-            return props.text[index]
-          }
-          return props.characterSet[getRandomInt(props.characterSet.length)]
-        }) as string[]
-
-        iterationCount.value += 0.1
-      }
-      else {
-        isAnimating.value = false
-      }
-    }, intervalDuration) as unknown as number
+    iterationCount.value = 0
+    displayText.value = props.text.split('')
+    totalCharacters = props.text.length
+    intervalMs.value = props.duration / (props.text.length * 10)
+    resume()
   }
   else {
-    if (intervalId) {
-      clearInterval(intervalId)
-      intervalId = null
-    }
-  }
-})
-
-onBeforeUnmount(() => {
-  if (intervalId) {
-    clearInterval(intervalId)
+    pause()
   }
 })
 </script>
