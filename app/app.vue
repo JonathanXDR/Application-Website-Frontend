f76ee6182c4cc4447431631749f3c5697b1b4646
Jonathan Russ <jonathan.russ@bluewin.ch>
2025-01-02 16:44:22 +0100

    feat(loading): add loading indicator

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,19 @@
 <template>
+  <NuxtLoadingIndicator
+    :color="color"
+    :error-color="`var(--color-figure-red)`"
+  />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const conifg = useRuntimeConfig()
+const { randomDevColor } = useColor()
+
+const color
+  = conifg.public.appEnvironment === 'development'
+    ? `var(--color-figure-${randomDevColor.value?.name})`
+    : 'var(--color-fill-blue)'
+</script>
