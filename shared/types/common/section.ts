1f089a758f82bc2d8f57746573aec50cfeda20c2
Jonathan Russ <jonathan.russ@bluewin.ch>
2025-01-05 00:10:32 +0100

    chore(hero): remove code hero label

--- shared/types/common/section.ts
+++ shared/types/common/section.ts
@@ -2,7 +2,7 @@ import type { IconType } from '#shared/types/common/icon'
 
 export interface SectionType {
   id: string
-  label: string
+  label?: string
   route: string
   children?: SectionType[]
   class?: string
