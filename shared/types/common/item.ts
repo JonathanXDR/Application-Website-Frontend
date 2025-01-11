ce2cf05da201cc749029ae4684f36e17414558f9
Jonathan Russ <jonathan.russ@bluewin.ch>
2025-01-11 16:23:29 +0100

    chore(types): cleanup some types

--- shared/types/common/item.ts
+++ shared/types/common/item.ts
@@ -1,8 +1,10 @@
 import type { IconType } from '#shared/types/common/icon'
+import type { LinkType } from '#shared/types/common/link'
 
 export interface ItemType {
   id: string
   category?: string
   label?: string
+  links?: LinkType[]
   icon?: IconType
 }
--- shared/types/common/responsive-breakpoints.ts
+++
@@ -1,9 +0,0 @@
-import type { Breakpoints } from '#shared/types/common/breakpoint'
-
-type ResponsiveSizeTypeTailwind<T extends string> = T | string
-type ResponsiveSizeType<T extends string> =
-  `${Breakpoints}:${ResponsiveSizeTypeTailwind<T>}`
-
-export type ResponsiveBreakpoints<T extends string> =
-  | ResponsiveSizeTypeTailwind<T>
-  | ResponsiveSizeType<T>
