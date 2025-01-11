44b6ea7899cf28477f4aea47014b1664a9d548bb
Jonathan Russ <jonathan.russ@bluewin.ch>
2025-01-11 12:06:57 +0100

    build(package): update dependencies

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 export default {
   '**/*.{js,jsx,ts,tsx,vue,json,css,scss,md}': stagedFiles => [
     `prettier --write ${stagedFiles.join(' ')}`,
-    `eslint --flag unstable_ts_config --fix ${stagedFiles.join(' ')}`,
+    `eslint --fix ${stagedFiles.join(' ')}`,
   ],
   '**/*.{ts,tsx,vue}': () => 'vue-tsc --noEmit',
 }
