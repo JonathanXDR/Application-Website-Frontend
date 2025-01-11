633c55b4badea5b54ac388196f9d86601bfe2569
Jonathan Russ <jonathan.russ@bluewin.ch>
2025-01-11 17:53:46 +0100

    feat(component): add ThemePicker

--- app/components/common/ColorSchemeToggle.vue
+++ app/components/common/ColorSchemeToggle.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import type { ItemType } from '#shared/types/common/item'
+import type { SegmentNavType } from '#shared/types/common/segment-nav'
+
+withDefaults(defineProps<Pick<SegmentNavType, 'label'>>(), {
+  label: 'text',
+})
 
 const { tm } = useI18n()
 const { getTheme, setTheme } = useTheme()
-const viewport = useViewport()
+const currentTheme = computed(() => getTheme())
 
 const themeItems = computed<ItemType[]>(() =>
   tm('components.common.SegmentNav.theme'),
@@ -11,13 +16,34 @@ const themeItems = computed<ItemType[]>(() =>
 </script>
 
 <template>
-  <SegmentNav
-    :items="themeItems"
-    gap="5px"
-    component-size="xsmall"
-    :focus="false"
-    :label="viewport.isGreaterOrEquals('desktop') ? 'text' : 'icon'"
-    :selected-item="getTheme()"
-    :on-select="(themeNew: string) => setTheme(themeNew)"
-  />
+  <div
+    class="color-scheme-toggle"
+    role="radiogroup"
+    tabindex="0"
+    aria-label="Select a color scheme preference"
+  >
+    <label
+      v-for="(item, index) in themeItems"
+      :key="index"
+      :for="item.id"
+    >
+      <input
+        :id="item.id"
+        type="radio"
+        autocomplete="off"
+        name="color-scheme"
+        :value="item.id"
+        :checked="currentTheme === item.id"
+        @change="setTheme(item.id)"
+      >
+      <div class="text">
+        <DynamicIcon
+          v-if="item.icon && label !== 'text'"
+          :name="item.icon.name"
+          class="icon icon-lg"
+        />
+        <div v-if="label !== 'icon'">{{ item.label }}</div>
+      </div>
+    </label>
+  </div>
 </template>
