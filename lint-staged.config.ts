c910ec58b2f192be3b53609bbd20fbafd10b6063
Jonathan Russ <jonathan.russ@bluewin.ch>
2025-01-22 20:19:30 +0100

    chore(config): use typescript for lint-staged

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,7 +1,11 @@
-export default {
+import type { Configuration } from 'lint-staged'
+
+const config: Configuration = {
   '**/*.{js,jsx,ts,tsx,vue,json,css,scss,md}': stagedFiles => [
     `prettier --write ${stagedFiles.join(' ')}`,
     `eslint --fix ${stagedFiles.join(' ')}`,
   ],
   '**/*.{ts,tsx,vue}': () => 'vue-tsc --noEmit',
 }
+
+export default config
