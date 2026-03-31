f2338d284d895eeee68afb7d6ae5fef97f29af14
Jonathan Russ <jonathan.russ@bluewin.ch>
2026-03-31 12:38:08 +0200

    feat: configure security secret for og-image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
     scripts: {
       globals: {
         meticulousAi: {
-          src: 'https://snippet.meticulous.ai/v1/meticulous.js',
+          'src': 'https://snippet.meticulous.ai/v1/meticulous.js',
           'data-project-id': '3xUUe4R1NNzA6BJE6HKzrGCjCRddpahZJeJh8N0w',
           'data-is-production-environment': false,
         },
@@ -64,7 +64,7 @@ export default defineNuxtConfig({
     scripts: {
       globals: {
         meticulousAi: {
-          src: 'https://snippet.meticulous.ai/v1/meticulous.js',
+          'src': 'https://snippet.meticulous.ai/v1/meticulous.js',
           'data-project-id': '3xUUe4R1NNzA6BJE6HKzrGCjCRddpahZJeJh8N0w',
           'data-is-production-environment': true,
         },
@@ -315,16 +315,21 @@ export default defineNuxtConfig({
     provider: 'vercel',
     screens: {
       '2xs': 320,
-      xs: 475,
-      sm: 640,
-      md: 768,
-      lg: 1024,
-      xl: 1280,
+      'xs': 475,
+      'sm': 640,
+      'md': 768,
+      'lg': 1024,
+      'xl': 1280,
       '2xl': 1536,
-      portrait: 411,
-      portrait2x: 822,
+      'portrait': 411,
+      'portrait2x': 822,
     },
     densities: [1, 2],
+  },
+  ogImage: {
+    security: {
+      secret: process.env.OG_IMAGE_SECRET,
+    },
   },
   robots: {
     disallow: ['/de/**', '/en/**', '/fr/**', '/it/**'],
@@ -354,8 +359,8 @@ export default defineNuxtConfig({
 
       worksFor: {
         '@type': 'Organization',
-        name: 'Swisscom',
-        url: 'https://www.swisscom.ch',
+        'name': 'Swisscom',
+        'url': 'https://www.swisscom.ch',
       },
     }),
   },
@@ -377,13 +382,13 @@ export default defineNuxtConfig({
     csrf: true,
     headers: {
       contentSecurityPolicy: {
-        'default-src': ["'self'", process.env.SITE_URL || ''],
-        'style-src': ["'self'", "'unsafe-inline'", process.env.SITE_URL || ''],
+        'default-src': ['\'self\'', process.env.SITE_URL || ''],
+        'style-src': ['\'self\'', '\'unsafe-inline\'', process.env.SITE_URL || ''],
         'script-src': [
-          "'self'",
-          "'strict-dynamic'",
-          "'nonce-{{nonce}}'",
-          "'wasm-unsafe-eval'",
+          '\'self\'',
+          '\'strict-dynamic\'',
+          '\'nonce-{{nonce}}\'',
+          '\'wasm-unsafe-eval\'',
           'https://*.googletagmanager.com',
           'https://snippet.meticulous.ai',
           'https://browser.sentry-cdn.com',
@@ -391,7 +396,7 @@ export default defineNuxtConfig({
           process.env.SITE_URL || '',
         ],
         'img-src': [
-          "'self'",
+          '\'self\'',
           'data:',
           'https://*.google-analytics.com',
           'https://*.analytics.google.com',
@@ -401,9 +406,9 @@ export default defineNuxtConfig({
           'https://*.apple.com',
           process.env.SITE_URL || '',
         ],
-        'frame-src': ["'self'", 'https://snippet.meticulous.ai'],
+        'frame-src': ['\'self\'', 'https://snippet.meticulous.ai'],
         'connect-src': [
-          "'self'",
+          '\'self\'',
           'https://*.google-analytics.com',
           'https://*.analytics.google.com',
           'https://*.googletagmanager.com',
