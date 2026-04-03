36dafadaf40f5e3910b90f75a417d3891b72b5d1
Jonathan Russ <jonathan.russ@bluewin.ch>
2026-04-03 21:02:02 +0200

    fix: add temporary type definitions for og-image utils

---
+++ app/og-image.d.ts
@@ -0,0 +1,5 @@
+declare module '#og-image/app/utils' {
+  import type { OgImageRuntimeConfig } from 'nuxt-og-image/dist/runtime/types'
+
+  export function useOgImageRuntimeConfig(): OgImageRuntimeConfig
+}
