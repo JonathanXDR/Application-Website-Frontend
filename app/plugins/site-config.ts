e42cbb86b857cd77ba113278e32366246422bfed
Jonathan Russ <jonathan.russ@bluewin.ch>
2026-04-24 23:40:00 +0200

    feat: prioritize page-specific site descriptions

--- app/plugins/site-config.ts
+++ app/plugins/site-config.ts
@@ -1,11 +1,19 @@
 export default defineNuxtPlugin(async () => {
+  const route = useRoute()
   const { data: siteContent } = await useQueryCollection('siteConfig')
     .stem('site')
     .first()
 
   watchEffect(() => {
-    if (siteContent.value?.description) {
-      updateSiteConfig({ description: siteContent.value.description })
-    }
+    if (!siteContent.value?.description) return
+
+    const pageName = (route.name as string)?.replace(/___\w+$/, '')
+    const pageDescription = pageName
+      ? siteContent.value.pages?.[pageName]?.description
+      : undefined
+
+    updateSiteConfig({
+      description: pageDescription || siteContent.value.description,
+    })
   })
 })
